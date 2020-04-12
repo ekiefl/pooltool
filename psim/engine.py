c51232110b35a57d87b9c02795702551adba5a9d
Evan Kiefl <kiefl.evan@gmail.com>
2020-04-12 11:52:34 -0500

    Slide state evolution is correct when intial wz == 0

--- psim/engine.py
+++ psim/engine.py
@@ -48,15 +48,79 @@ class Ball(object):
         self.history = {'t': [], 'rvw': [], 's': []}
 
 
-    def store(self, t, r, v, w, s):
+    def store(self, t, rvw, s):
         self.history['t'].append(t)
-        self.history['rvw'].append(np.array([r, v, w]))
+        self.history['rvw'].append(rvw)
         self.history['s'].append(s)
 
 
-    def update(self, r, v, w, s):
-        self.rvw = np.array([r, v, w])
+    def update(self, rvw, s):
+        self.rvw = rvw
         self.s = s
+
+
+    def plot_history(self, table):
+        """Primitive plotting for use during development"""
+        import pandas as pd
+        import matplotlib.pyplot as plt
+
+        t = np.array(self.history['t'])
+        s = np.array(self.history['s'])
+        rvw = np.array(self.history['rvw'])
+
+        df = pd.DataFrame({
+            'rx': rvw[:, 0, 0], 'ry': rvw[:, 0, 1], 'rz': rvw[:, 0, 2],
+            'vx': rvw[:, 1, 0], 'vy': rvw[:, 1, 1], 'vz': rvw[:, 1, 2],
+            'wx': rvw[:, 2, 0], 'wy': rvw[:, 2, 1], 'wz': rvw[:, 2, 2],
+            '|v|': np.sqrt(rvw[:, 1, 1]**2 + rvw[:, 1, 0]**2),
+            '|w|': np.sqrt(rvw[:, 2, 2]**2 + rvw[:, 2, 1]**2 + rvw[:, 2, 0]**2),
+            'time':t,
+            'state':s,
+        })
+
+        def add_plot(fig, num, x, y):
+            if np.max(np.abs(df[y])) < 0.000000001:
+                df[y] = 0
+
+            ax = fig.add_subplot(num)
+            for name, group in groups:
+                ax.plot(group[x], group[y], marker="o", linestyle="", label=name, ms=2.)
+            ax.set_ylabel(y)
+            ax.set_xlabel(x)
+            ax.legend()
+
+        groups = df.groupby('state')
+
+        # Table perspective
+        fig = plt.figure(figsize=(5, 10))
+        add_plot(fig, 111, 'rx', 'ry')
+        plt.title('rx vs ry')
+        plt.xlim(table.L, table.R)
+        plt.ylim(table.B, table.T)
+        plt.show()
+
+        fig = plt.figure(figsize=(6, 5))
+        add_plot(fig, 111, 'time', '|v|')
+        plt.tight_layout()
+        plt.show()
+
+        fig = plt.figure(figsize=(6, 5))
+        add_plot(fig, 111, 'time', '|w|')
+        plt.tight_layout()
+        plt.show()
+
+        fig = plt.figure(figsize=(10, 10))
+        add_plot(fig, 331, 'time', 'rx')
+        add_plot(fig, 332, 'time', 'ry')
+        add_plot(fig, 333, 'time', 'rz')
+        add_plot(fig, 334, 'time', 'vx')
+        add_plot(fig, 335, 'time', 'vy')
+        add_plot(fig, 336, 'time', 'vz')
+        add_plot(fig, 337, 'time', 'wx')
+        add_plot(fig, 338, 'time', 'wy')
+        add_plot(fig, 339, 'time', 'wz')
+        plt.tight_layout()
+        plt.show()
 
 
 class Cue(object):
@@ -65,7 +129,14 @@ class Cue(object):
         self.brand = brand
 
 
-    def strike(self, ball, V0, phi, theta, a, b):
+    def strike(self, ball, V0, phi, theta=None, a=None, b=None, sweet_spot=False):
+        if sweet_spot:
+            # b = 2/5 is the sweet spot for no sliding on table cloth
+            theta, a, b = 0, 0, 2/5
+        else:
+            if any([theta is None, a is None, b is None]):
+                raise ValueError("Cue.strike :: Must choose theta, a, and b")
+
         v_T, w_T = physics.cue_strike(ball.m, self.M, ball.R, V0, phi, theta, a, b)
 
         ball.rvw[1] = v_T
@@ -75,7 +146,7 @@ class Cue(object):
 
 class ShotSimulation(object):
     def __init__(self):
-        pass
+        self.t = 0
 
 
     def setup_test(self):
@@ -95,67 +166,43 @@ class ShotSimulation(object):
     def start(self):
         self.cue.strike(
             ball = self.balls['cue'],
-            V0 = 0.6,
+            V0 = 0.8,
             phi = 90,
-            theta = 20,
-            a = -0.5,
-            b = 0.0,
+            theta = 40,
+            a = 0.0,
+            b = -0.9,
         )
 
         q = self.balls['cue']
 
-        print(f"time of slide state: {physics.get_slide_time(q.rvw[1], q.rvw[2], R=q.R, u_s=self.table.u_s, g=psim.g)}")
-        print(f"time of spin state: {physics.get_spin_time(q.rvw[2], R=q.R, u_sp=self.table.u_sp, g=psim.g)}")
+        #for t in np.arange(0, 4, 0.1):
+        #    rvw, s = physics.evolve_ball_motion(
+        #        rvw=q.rvw,
+        #        R=q.R,
+        #        m=q.m,
+        #        u_s=self.table.u_s,
+        #        u_sp=self.table.u_sp,
+        #        u_r=self.table.u_r,
+        #        g=psim.g,
+        #        t=t,
+        #    )
+        #    q.store(t, rvw, s)
 
-        for t in np.arange(0, 10, 0.05):
-
-            r, v, w, s = physics.evolve_ball_motion(
-                *q.rvw,
+        slide_time = physics.get_slide_time(q.rvw, q.R, self.table.u_s, psim.g)
+        print(physics.get_rel_velocity(q.rvw, q.R))
+        rvw = q.rvw
+        t = 0
+        for dt in np.diff(np.linspace(0, slide_time, 50)):
+            rvw = physics.evolve_slide_state(
+                rvw=rvw,
                 R=q.R,
                 m=q.m,
                 u_s=self.table.u_s,
                 u_sp=self.table.u_sp,
-                u_r=self.table.u_r,
                 g=psim.g,
-                t=t,
+                t=dt,
             )
-
-            q.store(t, r, v, w, s)
-
-        import pandas as pd
-        import matplotlib.pyplot as plt
-
-        fig = plt.figure(figsize=(5, 10))
-        ax = fig.add_subplot(111)
-        ax.set_xlim(self.table.L, self.table.R)
-        ax.set_ylim(self.table.B, self.table.T)
-
-        rvw = np.array(q.history['rvw'])
-        x = rvw[:, 0, 0]
-        y = rvw[:, 0, 1]
-        s = np.array(q.history['s'])
-        df = pd.DataFrame({'x':x, 'y':y, 'state':s})
-
-        groups = df.groupby('state')
-        for name, group in groups:
-            ax.plot(group['x'], group['y'], marker="o", linestyle="", label=name, ms=2.)
-
-        ax.legend()
-        plt.show()
-
-        print(f"position after strike: {q.rvw[0]}")
-        print(f"position after evolve: {r_T}")
-        print()
-
-        print(f"velocity after strike: {q.rvw[1]}")
-        print(f"velocity after evolve: {v_T}")
-        print()
-
-        print(f"ang vel after strike: {q.rvw[2]}")
-        print(f"ang vel after evolve: {w_T}")
-        print()
-
-
-
-
+            t += dt
+            q.store(t, rvw, q.s)
+        q.plot_history(self.table)
 
--- psim/utils.py
+++ psim/utils.py
@@ -8,18 +8,26 @@ def unit_vector(vector):
 
 def angle(v2, v1=(1,0)):
     """Calculates counter-clockwise angle of the projections of v1 and v2 onto the x-y plane"""
+    ang = np.arctan2(v2[1], v2[0]) - np.arctan2(v1[1], v1[0])
 
-    ang2 = np.arctan2(v2[1], v2[0])
-    ang1 = np.arctan2(v1[1], v1[0])
+    if ang < 0:
+        return 2*np.pi + ang
 
-    return ang2 - ang1
+    return ang
 
 
 def coordinate_rotation(v, phi):
-    """Convert vector from one frame of reference to another (3D FIXME)"""
+    """Rotate vector/matrix from one frame of reference to another (3D FIXME)"""
+
+    if len(v.shape) == 1:
+        print(f"Input vector: {v}")
+        print(f"Input phi: {phi}")
 
     rotation = np.array([[np.cos(phi), -np.sin(phi), 0],
                          [np.sin(phi),  np.cos(phi), 0],
                          [0          ,  0          , 1]])
 
+    if len(v.shape) == 1:
+        print(f"Output vector: {np.matmul(rotation, v)}")
+        print()
     return np.matmul(rotation, v)
