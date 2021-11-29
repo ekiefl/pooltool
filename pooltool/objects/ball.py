YESYESYESYESYES calculate quaternions directly from angular velocity. For the first time, rotations look physically correct

pooltool/objects/ball.py
@@ -114,12 +114,12 @@ class BallRender(Render):
 
     def set_playback_sequence(self, playback_speed=1):
         """Creates the sequence motions of the ball for a given playback speed"""
-        # Get the trajectories
-        xyzs = autils.get_list_of_Vec3s_from_array(self.history.rvw[:, 0, :])
-        self.quats = autils.get_quaternion_list_from_array(utils.as_quaternion(self.history.rvw[:, 3, :]))
-
         dts = np.diff(self.history.t)
         playback_dts = dts/playback_speed
+
+        # Get the trajectories
+        xyzs = autils.get_list_of_Vec3s_from_array(self.history.rvw[:, 0, :])
+        self.quats = autils.as_quaternion(self.history.rvw[:, 2, :], self.history.t)
 
         # Init the sequences
         ball_sequence = Sequence()

pooltool/utils.py
@@ -124,22 +124,25 @@ def normalize_rotation_vector(v):
     return Rotation.from_rotvec(v).as_rotvec()
 
 
-def as_quaternion(v):
-    """Convert rotation vector to quaternion"""
-    n = v.shape[0]
-    quats = np.zeros((n, 4))
-    for i in range(n):
-        norm = np.linalg.norm(v[i,:])
-        if norm == 0:
-            quats[i, :] = np.array([1,0,0,0])
-            continue
-        quats[i, :] = Quaternion(axis=unit_vector(v[i,:]), angle=norm).normalised.elements
-    return quats
+def unit_vector(vector, ord=None, handle_zero=False):
+    """Returns the unit vector of the vector.
 
-
-def unit_vector(vector):
-    """Returns the unit vector of the vector."""
-    return vector / np.linalg.norm(vector)
+    Parameters
+    ==========
+    ord: None
+        The type of normalization used. See
+        https://numpy.org/doc/stable/reference/generated/numpy.linalg.norm.html
+    handle_zero: bool, False
+        If True and vector = <0,0,0>, <0,0,0> is returned.
+    """
+    if len(vector.shape) > 1:
+        norm = np.linalg.norm(vector, ord=ord, axis=1, keepdims=True)
+        if handle_zero:
+            norm[(norm == 0).all(axis=1), :] = 1
+        return vector / norm
+    else:
+        norm = np.linalg.norm(vector, ord=ord)
+        return vector / (norm if not handle_zero else 1)
 
 
 def angle(v2, v1=(1,0)):