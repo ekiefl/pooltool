3d21c2fc4ac40037b1e2274b12f2f38faaee2da3
Evan Kiefl <kiefl.evan@gmail.com>
2021-12-05 21:23:47 -0600

    Explain the philosophy of test_physics.py. Add ball-ball and ball-cushion tests

--- pooltool/tests/unit/test_physics.py
+++ pooltool/tests/unit/test_physics.py
@@ -1,18 +1,78 @@
 #! /usr/bin/env python
 """Unit testing for physics.py
 
-FIXME My plan for this module is to test each of the functions in physics.py by taking the reference shot
-(ref) and parsing its events. For example, resolve_ball_ball_collision could be tested by collecting
-every ball-ball collision event in ref, and then creating a new BallBallCollisionEvent with two balls
-with states event.ball1_state_start, event.ball2_state_start. Then, by calling event.resolve, I can
-ensure event.ball1_state_end, and event.ball2_state.end are produced. The reason this hasn't yet happened
-is that I need to create a pickleable representation for the Event object, so that saving/loading/copying
-system states preserve event history.
+Most of the unit tests in this module work differently than one might expect. Rather than feed each
+physics equation a series of carefully crafted examples where the expected answer is calculated by
+hand, instead I search for existing examples of the physics equation being used within the benchmark
+shot.  Since the benchmark shot is assumed to be the truth, I can test whether the function in the
+current codebase returns the identical output to that in the benchmark shot.
+
+The advantages of this approach is that (1) the tests require very little upkeep and (2) a large
+breadth of circumstances are tested for each physics equation. For example, at the time of writing
+this, the benchmark shot contains 18 ball-ball collisions, so physics.resolve_ball_ball_collision is
+tested under 18 non-trivial circumstances.
+
+The disadvantage to this approach is that truth is defined relative to the benchmark shot. If the
+benchmark shot contains incorrect functionality, then these tests serve no purpose in ensuring
+absolute truth. With this in mind, these unit tests are not designed to prove correctness, but
+rather to preserve functionality in the face of any refactoring that takes place.
 """
 
 import pooltool.physics as p
 
 from pooltool.tests import trial, ref
 
+import numpy as np
+
+
 def test_resolve_ball_ball_collision(ref):
-    pass
+    collision_events = ref.events.filter_type('ball-ball')
+
+    for col in collision_events:
+        ball1, ball2 = col.agents
+
+        # Set agents to their pre-resolved states
+        ball1.rvw, ball1.s = col.agent1_state_initial
+        ball2.rvw, ball2.s = col.agent2_state_initial
+
+        # Store the expected post-resolved states. These were the values
+        # calculated from the reference benchmark, which we assume to be
+        # the correct values
+        ball1_rvw_expected, ball1_s_expected = np.copy(col.agent1_state_final[0]), col.agent1_state_final[1]
+        ball2_rvw_expected, ball2_s_expected = np.copy(col.agent2_state_final[0]), col.agent2_state_final[1]
+
+        # Now resolve the event with the current codebase
+        col.resolve()
+        ball1_rvw, ball1_s = col.agent1_state_final
+        ball2_rvw, ball2_s = col.agent2_state_final
+
+        # Assert the calculated values equal the reference values
+        np.testing.assert_allclose(ball1_rvw, ball1_rvw_expected)
+        np.testing.assert_allclose(ball1_s, ball1_s_expected)
+        np.testing.assert_allclose(ball2_rvw, ball2_rvw_expected)
+        np.testing.assert_allclose(ball2_s, ball2_s_expected)
+
+
+def test_resolve_ball_cushion_collision(ref):
+    collision_events = ref.events.filter_type('ball-cushion')
+
+    for col in collision_events:
+        ball, cushion = col.agents
+
+        # Set agents to their pre-resolved states
+        ball.rvw, ball.s = col.agent1_state_initial
+
+        # Store the expected post-resolved states. These were the values
+        # calculated from the reference benchmark, which we assume to be
+        # the correct values
+        ball_rvw_expected, ball_s_expected = np.copy(col.agent1_state_final[0]), col.agent1_state_final[1]
+
+        # Now resolve the event with the current codebase
+        col.resolve()
+        ball_rvw, ball_s = col.agent1_state_final
+
+        # Assert the calculated values equal the reference values
+        np.testing.assert_allclose(ball_rvw, ball_rvw_expected)
+        np.testing.assert_allclose(ball_s, ball_s_expected)
+
+
