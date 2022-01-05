a543dea976e1cf9b42c8795af68ca337257a081a
Evan Kiefl <kiefl.evan@gmail.com>
2022-01-04 16:14:06 -0800

    Add new sandbox script to compare timings of calculating, continuizing, and rendering

---
+++ sandbox/calculate_continuize_render_times.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import pooltool as pt
+
+def main(args):
+    shot = pt.System(path=args.path)
+
+    shot.simulate(quiet=True)
+    shot.reset_balls()
+
+    with pt.terminal.TimeCode(success_msg = 'Trajectories simulated in: '):
+        shot.simulate(quiet=True)
+
+    with pt.terminal.TimeCode(success_msg = 'Trajectories continuized in: '):
+        shot.continuize(dt=1/60*2.5)
+
+    class Interface(pt.ShotViewer):
+        def __init__(self, *args, **kwargs):
+            pt.ShotViewer.__init__(self, *args, **kwargs)
+
+        def change_mode(self, *args, **kwargs):
+            with pt.terminal.TimeCode(success_msg = 'Animation sequence rendered in: '):
+                super().change_mode(*args, **kwargs)
+
+    interface = Interface()
+    interface.show(shot)
+
+if __name__ == '__main__':
+    import argparse
+
+    ap = argparse.ArgumentParser('Compare timings of shot calculation, continuization, and animation rendering')
+    ap.add_argument('--path', type=str, required=True, help="Filepath to a shot (you could generate this with sandbox/break.py, for example)")
+
+    args = ap.parse_args()
+
+    main(args)
