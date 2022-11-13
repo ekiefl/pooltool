80b5bfb7b9a355b087580da52d207cde90158fff
Evan Kiefl <kiefl.evan@gmail.com>
2022-11-12 18:15:04 -0800

    Feels redundant to do this

--- pooltool/ani/tasks.py
+++ pooltool/ani/tasks.py
@@ -1,23 +1,19 @@
-from pooltool.ani.globals import Global, require_showbase
+from pooltool.ani.globals import Global
 
 
-@require_showbase
 def has(name):
     return Global.task_mgr.hasTaskNamed(name)
 
 
-@require_showbase
 def add(func, name, *args, **kwargs):
     if not has(name):
         # If the task already exists, don't add it again
         Global.task_mgr.add(func, name, *args, **kwargs)
 
 
-@require_showbase
 def add_later(*args, **kwargs):
     Global.task_mgr.doMethodLater(*args, **kwargs)
 
 
-@require_showbase
 def remove(name):
     Global.task_mgr.remove(name)
