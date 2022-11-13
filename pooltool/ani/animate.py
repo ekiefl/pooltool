033632a5473aca91e8f7b92ec00dc71a2e0f42ee
Evan Kiefl <kiefl.evan@gmail.com>
2022-11-12 21:04:05 -0800

    Unneeded, initialized in Interface

--- pooltool/ani/animate.py
+++ pooltool/ani/animate.py
@@ -484,7 +484,6 @@ class Play(Interface):
     def go(self):
         menus.hide_all()
 
-        self.shots = SystemCollection()
         self.shots.append(System())
         self.shots.set_active(-1)
 
