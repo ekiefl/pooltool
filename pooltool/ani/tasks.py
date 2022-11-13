81a03f29ac76a7d6be03024298d026bb39ad9331
Evan Kiefl <kiefl.evan@gmail.com>
2022-11-13 00:11:54 -0800

    Juice the register_event docstring

--- pooltool/ani/tasks.py
+++ pooltool/ani/tasks.py
@@ -24,7 +24,31 @@ def remove(name):
 
 
 @require_showbase
-def register_event(keystroke, func, func_args=[]):
-    """Register event listener that triggers based on keystroke/mouse"""
+def register_event(sequence, func, func_args=[]):
+    """Register event listener that triggers based on keystroke/mouse/message
 
-    Global.base.accept(keystroke, func, func_args)
+    Args:
+        sequence:
+            Sequence can be (1) a key specifier, (2) a mouse click specifier, or (3) a
+            custom string.
+
+            (1): A character in [0-9a-z], or a keyname in {"escape", "backspace",
+                 "insert", "home", "page_up", "num_lock", "tab", "delete", "end",
+                 "page_down", "caps_lock", "enter", "arrow_left", "arrow_up",
+                 "arrow_down", "arrow_right", "shift", "lshift", "rshift", "conrol",
+                 "alt", "lcontrol", "window-event", "lalt", "space", "ralt",
+                 "rcontrol"}. Each key can be prefixed with modifier keys that can
+                 chain, e.g. "shift-control-alt-a", and can be suffixed with "-down",
+                 "-up", or "-repeat".
+
+            (2): Any of {"mouse1", "mouse2", "mouse3", "wheel_up", "wheel_down"}. Can be
+                 suffixed with "-down" and "-up".
+
+            (3): A custom string. The event will be triggered when a message containing
+                 the string is sent. E.g. register_event("test", lambda: print("hi"))
+                 would print "hi" when the following call was made:
+
+                    Global.base.messenger.send("test")
+    """
+
+    Global.base.accept(sequence, func, func_args)
