70c47beb718216839d9f027009c49895f476c516
Evan Kiefl <kiefl.evan@gmail.com>
2023-02-22 18:28:52 -0800

    Rework table component copy methods with read-only
    - This cuts down on needless object creation and numpy copying, since
      these objects are continually copied as snapshots of initial and final
      Agent states

--- pooltool/objects/table/test_components.py
+++ pooltool/objects/table/test_components.py
@@ -57,12 +57,21 @@ def test_linear_segment_frozen(lin_seg):
     with pytest.raises(ValueError, match="assignment destination is read-only"):
         lin_seg.p1[0] = 10
 
+    # Not bulletproof though. One can set the WRITABLE flag
+    lin_seg.p1.flags["WRITEABLE"] = True
+    lin_seg.p1[0] = 10
+
 
 def test_linear_segment_copy(lin_seg):
     copy = lin_seg.copy()
 
     # lin_seg and copy equate
     assert lin_seg == copy
+
+    # In fact, lin_seg and copy are the same object. We can get away with this because
+    # it's a frozen object with either (a) immutable attributes or (b) attributes which
+    # with read-only flags set
+    assert lin_seg is copy
 
 
 def test_circular_segment_creation():
@@ -80,7 +89,12 @@ def test_circular_segment_frozen(circ_seg):
     with pytest.raises(FrozenInstanceError):
         circ_seg.center = np.array([10, 10, 10])
 
-    # Unfortunately, this is allowed
+    # Cannot even modify array elements
+    with pytest.raises(ValueError, match="assignment destination is read-only"):
+        circ_seg.center[0] = 10
+
+    # Not bulletproof though. One can set the WRITABLE flag
+    circ_seg.center.flags["WRITEABLE"] = True
     circ_seg.center[0] = 10
 
 
@@ -88,12 +102,12 @@ def test_circular_segment_copy(circ_seg):
     copy = circ_seg.copy()
 
     # circ_seg and copy equate
-    assert circ_seg is not copy
     assert circ_seg == copy
 
-    # modifying circ_seg doesn't affect copy
-    circ_seg.center[0] = 10
-    assert circ_seg != copy
+    # In fact, circ_seg and copy are the same object. We can get away with this because
+    # it's a frozen object with either (a) immutable attributes or (b) attributes which
+    # with read-only flags set
+    assert circ_seg is copy
 
 
 def test_pocket_creation():
@@ -121,26 +135,38 @@ def test_pocket_creation():
     )
 
 
-def test_pocket_frozen(pocket):
+def test_pocket_modifiability(pocket):
+    """Test what is and isn't modifiable for Pocket"""
     # Cannot set attributes of frozen class
     with pytest.raises(FrozenInstanceError):
         pocket.center = np.array([10, 10, 10])
+    with pytest.raises(FrozenInstanceError):
+        pocket.contains = set()
 
-    # Unfortunately, this is allowed
+    # Cannot modify array elements
+    with pytest.raises(ValueError, match="assignment destination is read-only"):
+        pocket.center[0] = 10
+
+    # Not bulletproof though. One can set the WRITABLE flag
+    pocket.center.flags["WRITEABLE"] = True
     pocket.center[0] = 10
+
+    # One can however, add and subtract from `contains`
+    pocket.contains.add("dummy")
 
 
 def test_pocket_copy(pocket):
     copy = pocket.copy()
 
     # pocket and copy equate
-    assert pocket.contains is not copy.contains
-    assert pocket is not copy
     assert pocket == copy
 
-    # modifying pocket doesn't affect copy
-    pocket.center[0] = 10
-    assert pocket != copy
+    # center is read only, so its safe that they share the same reference
+    pocket.center is copy.center
+
+    # contains is mutable, so separate objects is necessary
+    assert pocket.contains == copy.contains
+    assert pocket.contains is not copy.contains
 
 
 def test_cushion_segments_copy(lin_seg, circ_seg):
@@ -150,9 +176,16 @@ def test_cushion_segments_copy(lin_seg, circ_seg):
     copy = segments.copy()
 
     # segments and copy equate
-    assert segments is not copy
     assert segments == copy
 
     # Their dictionaries are not the same
     assert segments.linear is not copy.linear
     assert segments.circular is not copy.circular
+
+    # You can't change the elements within a dictionary
+    with pytest.raises(ValueError, match="assignment destination is read-only"):
+        segments.linear[lin_seg.id].p1[0] = 4
+
+    # But you can add new elements to `copy` without changing `segments`
+    copy.linear["new"] = circ_seg
+    assert "new" not in segments.linear
