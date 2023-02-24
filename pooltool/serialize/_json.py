8c7c68f6ce447a9564dd28fc83456ed094734cef
Evan Kiefl <kiefl.evan@gmail.com>
2023-02-23 21:46:16 -0800

    Add msgpack to the mix

--- pooltool/serialize/_json.py
+++ pooltool/serialize/_json.py
@@ -1,12 +1,14 @@
 import json
-from pathlib import Path
+from typing import Any
+
+from pooltool.serialize.types import Pathish
 
 
-def to_json(dictionary: dict, filepath: Path) -> None:
-    with open(filepath, "w") as outfile:
-        json.dump(dictionary, outfile, indent=4)
+def to_json(o: Any, path: Pathish) -> None:
+    with open(path, "w") as fp:
+        json.dump(o, fp, indent=4)
 
 
-def from_json(filepath: Path) -> dict:
-    with open(filepath, "r") as openfile:
-        return json.load(openfile)
+def from_json(path: Pathish) -> Any:
+    with open(path, "r") as fp:
+        return json.load(fp)
