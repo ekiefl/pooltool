c54de5fccf79c8662fd242ed31015f4d22d7c342
Evan Kiefl <kiefl.evan@gmail.com>
2023-03-01 18:56:07 -0800

    This is an unquantified time saver

--- pooltool/serialize/__init__.py
+++ pooltool/serialize/__init__.py
@@ -1,5 +1,5 @@
 from pooltool.serialize.convert import Convert
-from pooltool.serialize.serializers import SerializeFormat
+from pooltool.serialize.serializers import SerializeFormat, to_json, to_msgpack, from_json, from_msgpack
 import numpy as np
 from cattrs.preconf.json import make_converter as make_json_converter
 from cattrs.preconf.msgpack import make_converter as make_msgpack_converter
@@ -14,10 +14,17 @@ conversion = Convert(
 
 # Numpy arrays
 # https://github.com/python-attrs/cattrs/issues/194
-# FIXME does MSGPACK need this?
 conversion.register_structure_hook_func(
     lambda t: getattr(t, "__origin__", None) is np.ndarray,
     lambda v, t: np.array([t.__args__[1].__args__[0](e) for e in v]),
+    which=(SerializeFormat.JSON,),
+)
+
+# msgpack arrays are already arrays, simply return oneself
+conversion.register_structure_hook_func(
+    lambda t: getattr(t, "__origin__", None) is np.ndarray,
+    lambda v, t: v,
+    which=(SerializeFormat.MSGPACK,),
 )
 
 # JSON needs to unstructure numpy arrays as lists
