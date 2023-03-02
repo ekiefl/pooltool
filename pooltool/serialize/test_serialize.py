0342ce1d5a086a7b029f25709374d7487b102139
Evan Kiefl <kiefl.evan@gmail.com>
2023-03-01 18:56:43 -0800

    Rewrite serialize tests

--- pooltool/serialize/test_serialize.py
+++ pooltool/serialize/test_serialize.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+import filecmp
 import numpy as np
+from pathlib import Path
 import pytest
 from attrs import define
 from numpy.typing import NDArray
+import tempfile
 
-from pooltool.serialize import converter
+from pooltool.serialize import conversion, SerializeFormat, to_json, from_json, to_msgpack, from_msgpack
 from pooltool.utils.dataclasses import are_dataclasses_equal
 from pooltool.utils.strenum import StrEnum, auto
 
@@ -16,7 +20,7 @@ class ImNested:
 
 
 @define
-class StandardTypes:
+class SimpleObj:
     a: ImNested
     b: float
     c: set
@@ -24,8 +28,11 @@ class StandardTypes:
 
 
 @define(eq=False)
-class WithNumpyArray:
-    array: NDArray[np.float64]
+class ComplexObj(SimpleObj):
+    float64_array: NDArray[np.float64]
+    float32_array: NDArray[np.float32]
+    int32_array: NDArray[np.int32]
+    an_enum: Categories
 
     def __eq__(self, other):
         return are_dataclasses_equal(self, other)
@@ -38,8 +45,9 @@ class Categories(StrEnum):
 
 
 @pytest.fixture
-def standard_obj():
-    return StandardTypes(
+def simple_obj():
+    """Contains only simple dtypes and attrs classes"""
+    return SimpleObj(
         a=ImNested(a=True, b=42, c="foo"),
         b=47.0,
         c={1, 2, 3},
@@ -48,41 +56,201 @@ def standard_obj():
 
 
 @pytest.fixture
-def numpy_obj(standard_obj):
-    return WithNumpyArray(
-        a=standard_obj.a,
-        b=standard_obj.b,
-        c=standard_obj.c,
-        d=standard_obj.d,
-        e=np.arange(9, dtype=np.float64).reshape((3, 3)),
+def complex_obj(simple_obj):
+    return ComplexObj(
+        a=simple_obj.a,
+        b=simple_obj.b,
+        c=simple_obj.c,
+        d=simple_obj.d,
+        float64_array=np.arange(9, dtype=np.float64).reshape((3, 3)),
+        float32_array=np.arange(9, dtype=np.float32).reshape((3, 3)),
+        int32_array=np.arange(9, dtype=np.int32).reshape((3, 3)),
+        an_enum=Categories.three,
     )
 
 
-def test_standard(standard_obj):
-    """Test unstructure/structure round trip with standard types"""
-    assert standard_obj == converter.structure(
-        converter.unstructure(standard_obj), StandardTypes
-    )
-    assert numpy_obj == converter.structure(
-        converter.unstructure(numpy_obj), WithNumpyArray
-    )
+# --------------------------------------------------------------------------------------
 
 
-def test_numpy(numpy_obj):
-    """Test unstructure/structure round trip with numpy types"""
-    assert numpy_obj == converter.structure(
-        converter.unstructure(numpy_obj), WithNumpyArray
-    )
+@pytest.mark.parametrize("fmt", [SerializeFormat.JSON, SerializeFormat.MSGPACK])
+def test_round_python_trip(simple_obj, complex_obj, fmt: SerializeFormat):
+    """Round trip: structure -> unstructure -> structure"""
+    c = conversion[fmt]
+    assert c.structure(c.unstructure(simple_obj), SimpleObj) == simple_obj
+    assert c.structure(c.unstructure(complex_obj), ComplexObj) == complex_obj
 
 
-def test_strenum():
-    """Test unstructure/structure round trip with strenum subclasses"""
-    # Unstructured StrEnum is a string but not a StrEnum
-    unstruct = converter.unstructure(Categories.one)
-    assert isinstance(unstruct, str)
-    assert not isinstance(unstruct, Categories)
+@pytest.mark.parametrize("fmt", [SerializeFormat.JSON, SerializeFormat.MSGPACK])
+def test_round_filesystem_trip(simple_obj, complex_obj, fmt: SerializeFormat):
+    """Round trip: structure -> unstructure -> filesystem -> unstructure -> structure"""
 
-    # StrEnum structured from string is a string and a StrEnum
-    struct = converter.structure("two", Categories)
-    assert isinstance(struct, str)
-    assert isinstance(struct, Categories)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        path = Path(tmp_dir) / f"tmp.{fmt.ext}"
+
+        # Write to file
+        conversion.unstructure_to(simple_obj, path, fmt)
+
+        # Read from file
+        assert simple_obj == conversion.structure_from(path, SimpleObj, fmt)
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        path = Path(tmp_dir) / f"tmp.{fmt.ext}"
+
+        # Write to file
+        conversion.unstructure_to(complex_obj, path, fmt)
+
+        # Read from file
+        assert complex_obj == conversion.structure_from(path, ComplexObj, fmt)
+
+
+def test_unstructure_json(complex_obj):
+    c = conversion[SerializeFormat.JSON]
+    uo = c.unstructure(complex_obj)
+
+    # Arrays are nested lists
+    assert isinstance(uo["float64_array"], list)
+    assert isinstance(uo["float64_array"][0], list)
+    assert isinstance(uo["float64_array"][0][0], float)
+
+    assert isinstance(uo["float32_array"], list)
+    assert isinstance(uo["float32_array"][0], list)
+    assert isinstance(uo["float32_array"][0][0], float)
+
+    assert isinstance(uo["int32_array"], list)
+    assert isinstance(uo["int32_array"][0], list)
+    assert isinstance(uo["int32_array"][0][0], int)
+
+    # Enums are strs and not Enums
+    assert isinstance(uo["an_enum"], str)
+    assert not isinstance(uo["an_enum"], Categories)
+
+
+def test_unstructure_to_json(complex_obj):
+    fmt = SerializeFormat.JSON
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        method_path = Path(tmp_dir) / f"method.{fmt.ext}"
+        manual_path = Path(tmp_dir) / f"manual.{fmt.ext}"
+
+        # Write with unstructure_to
+        conversion.unstructure_to(complex_obj, method_path, fmt)
+
+        # Write manually
+        uo = conversion[fmt].unstructure(complex_obj)
+        to_json(uo, manual_path)
+
+        # Files are equal
+        assert filecmp.cmp(method_path, manual_path, shallow=False)
+
+
+def test_structure_json(complex_obj):
+    c = conversion[SerializeFormat.JSON]
+    uo = c.unstructure(complex_obj)
+    o = c.structure(uo, ComplexObj)
+
+    # Array types are persistent
+    assert o.float64_array.dtype == np.float64
+    assert o.float32_array.dtype == np.float32
+    assert o.int32_array.dtype == np.int32
+
+    # Enums are Enums
+    assert isinstance(o.an_enum, Categories)
+
+
+def test_structure_from_json(complex_obj):
+    fmt = SerializeFormat.JSON
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        path = Path(tmp_dir) / f"method.{fmt.ext}"
+        conversion.unstructure_to(complex_obj, path, fmt)
+
+        # Read with structure_from
+        output = conversion.structure_from(path, ComplexObj, fmt)
+
+        # Read manually
+        output_manual = conversion[fmt].structure(from_json(path), ComplexObj)
+
+    assert output == output_manual
+
+    # Array types are persistent
+    assert output.float64_array.dtype == np.float64
+    assert output.float32_array.dtype == np.float32
+    assert output.int32_array.dtype == np.int32
+
+    # Enums are Enums
+    assert isinstance(output.an_enum, Categories)
+
+
+def test_unstructure_msgpack(complex_obj):
+    c = conversion[SerializeFormat.MSGPACK]
+    uo = c.unstructure(complex_obj)
+
+    # Arrays are kept as arrays
+    assert isinstance(uo["float64_array"], np.ndarray)
+    assert isinstance(uo["float32_array"], np.ndarray)
+    assert isinstance(uo["int32_array"], np.ndarray)
+
+    # Not only that, but they are the same object
+    assert uo["float64_array"] is complex_obj.float64_array
+    assert uo["float32_array"] is complex_obj.float32_array
+    assert uo["int32_array"] is complex_obj.int32_array
+
+    # Enums are strs and not Enums
+    assert isinstance(uo["an_enum"], str)
+    assert not isinstance(uo["an_enum"], Categories)
+
+
+def test_unstructure_to_msgpack(complex_obj):
+    fmt = SerializeFormat.MSGPACK
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        method_path = Path(tmp_dir) / f"method.{fmt.ext}"
+        manual_path = Path(tmp_dir) / f"manual.{fmt.ext}"
+
+        # Write with unstructure_to
+        conversion.unstructure_to(complex_obj, method_path, fmt)
+
+        # Write manually
+        uo = conversion[fmt].unstructure(complex_obj)
+        to_msgpack(uo, manual_path)
+
+        # Files are equal
+        assert filecmp.cmp(method_path, manual_path, shallow=False)
+
+
+def test_structure_msgpack(complex_obj):
+    c = conversion[SerializeFormat.MSGPACK]
+    uo = c.unstructure(complex_obj)
+    o = c.structure(uo, ComplexObj)
+
+    # Array types are persistent
+    assert o.float64_array.dtype == np.float64
+    assert o.float32_array.dtype == np.float32
+    assert o.int32_array.dtype == np.int32
+
+    # Enums are Enums
+    assert isinstance(o.an_enum, Categories)
+
+
+def test_structure_from_msgpack(complex_obj):
+    fmt = SerializeFormat.MSGPACK
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        path = Path(tmp_dir) / f"method.{fmt.ext}"
+        conversion.unstructure_to(complex_obj, path, fmt)
+
+        # Read with structure_from
+        output = conversion.structure_from(path, ComplexObj, fmt)
+
+        # Read manually
+        output_manual = conversion[fmt].structure(from_msgpack(path), ComplexObj)
+
+    assert output == output_manual
+
+    # Array types are persistent
+    assert output.float64_array.dtype == np.float64
+    assert output.float32_array.dtype == np.float32
+    assert output.int32_array.dtype == np.int32
+
+    # Enums are Enums
+    assert isinstance(output.an_enum, Categories)
