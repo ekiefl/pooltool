39c07b55c8ef2673c74c4b0835b1bf8aa67ff40b
Evan Kiefl <kiefl.evan@gmail.com>
2023-03-10 22:40:30 -0800

    Add YAML support

--- pooltool/serialize/__init__.py
+++ pooltool/serialize/__init__.py
@@ -1,10 +1,14 @@
 import numpy as np
 from cattrs.preconf.json import make_converter as make_json_converter
 from cattrs.preconf.msgpack import make_converter as make_msgpack_converter
+from cattrs.preconf.pyyaml import make_converter as make_yaml_converter
 
 from pooltool.serialize.convert import Convert
 from pooltool.serialize.serializers import (
+    Pathish,
     SerializeFormat,
+    from_yaml,
+    to_yaml,
     from_json,
     from_msgpack,
     to_json,
@@ -16,6 +20,7 @@ conversion = Convert(
     {
         SerializeFormat.JSON: make_json_converter(),
         SerializeFormat.MSGPACK: make_msgpack_converter(),
+        SerializeFormat.YAML: make_yaml_converter(),
     }
 )
 
@@ -53,6 +58,8 @@ __all__ = [
     "SerializeFormat",
     "to_json",
     "to_msgpack",
+    "to_yaml",
     "from_json",
     "from_msgpack",
+    "from_yaml",
 ]
--- pooltool/serialize/serializers.py
+++ pooltool/serialize/serializers.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import yaml
 import msgpack
 import msgpack_numpy as m
 from typing import Union, Any, Dict, Callable
@@ -11,6 +12,7 @@ Pathish = Union[str, Path]
 class SerializeFormat(StrEnum):
     JSON = auto()
     MSGPACK = auto()
+    YAML = auto()
 
     @property
     def ext(self):
@@ -27,6 +29,16 @@ def from_json(path: Pathish) -> Any:
         return json.load(fp)
 
 
+def to_yaml(o: Any, path: Pathish) -> None:
+    with open(path, "w") as fp:
+        yaml.dump(o, fp)
+
+
+def from_yaml(path: Pathish) -> Any:
+    with open(path, "r") as fp:
+        return yaml.safe_load(fp)
+
+
 def to_msgpack(o: Any, path: Pathish) -> None:
     with open(path, "wb") as fp:
         fp.write(msgpack.packb(o, default=m.encode))
@@ -40,9 +52,11 @@ def from_msgpack(path: Pathish) -> Any:
 serializers: Dict[SerializeFormat, Callable[[Any, Pathish], None]] = {
     SerializeFormat.JSON: to_json,
     SerializeFormat.MSGPACK: to_msgpack,
+    SerializeFormat.YAML: to_yaml,
 }
 
 deserializers: Dict[SerializeFormat, Callable[[Pathish], Any]] = {
     SerializeFormat.JSON: from_json,
     SerializeFormat.MSGPACK: from_msgpack,
+    SerializeFormat.YAML: from_yaml,
 }
