339c903b3c0988024041e11dd243b626f62c66f1
Evan Kiefl <kiefl.evan@gmail.com>
2023-05-30 17:15:00 -0700

    Deviation happens at the start of a next shot

--- sandbox/z_drift_debug.py
+++ sandbox/z_drift_debug.py
@@ -5,20 +5,12 @@ BROKEN_MULTISYSTEM = "debug.msgpack"
 
 multisystem = MultiSystem.load(BROKEN_MULTISYSTEM)
 
-idx = 0
 for system in multisystem:
-    for ball in system.balls.values():
-        for state in ball.history:
-            # We don't care about pocketed states
-            if state.s == pocketed:
-                continue
-
-            # Does the ball's z position equal it's radius?
-            if state.rvw[0, 2] != ball.params.R:
-                print(idx, ball.id)
-                break
-
-    idx += 1
+    for state in system.balls["cue"].history:
+        if state.s == pocketed:
+            print("pocketed")
+            continue
+        print(state.rvw[0, 2])
 
 interface = ShotViewer()
 interface.show(multisystem[4])
