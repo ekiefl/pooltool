631f53490d5b6099ef330d531c464e977dfbcce8
Evan Kiefl <kiefl.evan@gmail.com>
2023-06-11 19:27:36 -0700

    Trialing hybrid approach to imaginary tolerance
    - Whether or not this remains will be determined by the test suite I'm
      developing in `test_simulate.py`

--- pooltool/math/roots/__init__.py
+++ pooltool/math/roots/__init__.py
@@ -19,9 +19,13 @@ _routine: Dict[QuarticSolver, Callable] = {
 
 
 def min_real_root(
-    ps: NDArray[np.float64], solver: QuarticSolver = QuarticSolver.NUMERIC, tol=1e-9
+    ps: NDArray[np.float64],
+    solver: QuarticSolver = QuarticSolver.NUMERIC,
+    abs_or_rel_cutoff: float = 1e-3,
+    rtol: float = 1e-3,
+    atol: float = 1e-9,
 ) -> Tuple[float, int]:
-    """Given an array of polynomial coefficients, find the minimum real root
+    """Given an array of polynomial coefficients, find the minimum, real, positive root
 
     Parameters
     ==========
@@ -34,8 +38,22 @@ def min_real_root(
         How should the roots be calculated? Choose one of {"numeric",
         "quartic_analytic"}. "numeric" works for n-degree polynomials,
         "quartic_analytic" works for quartics and is fast.
-    tol:
-        Roots are real if their imaginary components are less than than tol.
+    abs_or_rel_cutoff:
+        The criteria for a root being real depends on the magnitude of it's real
+        component. If it's large, we require the imaginary component is less than atol
+        in absolute terms. But when the real component is small, we require the
+        imaginary component be less than a fraction, rtol, of the real component. This
+        is because when the real component is small, perhaps even comparable to atol,
+        using an absolute cutoff for the imaginary component doesn't make much sense.
+        abs_or_rel_cutoff defines a threshold for the magnitude of the real component,
+        above which atol is used and below which rtol is used.
+    atol:
+        A root r (with abs(r.real) >= abs_or_rel_cutoff) is considered real if
+        abs(r.imag) < atol.
+    rtol:
+        A root r (with abs(r.real) < abs_or_rel_cutoff) is considered real if
+        abs(r.imag) / abs(r.real) < rtol. And in the special case when r.real == 0, the
+        root is considered real if r.imag == 0, too.
 
     Returns
     =======
@@ -48,9 +66,20 @@ def min_real_root(
     assert QuarticSolver(solver)
     times = _routine[solver](ps)
 
-    # If the root has a nonzero imaginary component, set to infinity
-    # If the root has a nonpositive real component, set to infinity
-    times[(abs(times.imag) > tol) | (times.real <= tol)] = np.inf
+    # FIXME This should be 0 eventually, not 1e-9
+    negative = times.real < 1e-9
+
+    imag_mag = np.abs(times.imag)
+    real_mag = np.abs(times.real)
+
+    big_mask = (imag_mag > atol) | negative
+
+    small_discard1 = ((imag_mag / real_mag) > rtol) & (real_mag > 0)
+    small_discard2 = (imag_mag != 0) & (real_mag == 0)
+    small_mask = small_discard1 | small_discard2 | negative
+
+    times[big_mask & (real_mag > abs_or_rel_cutoff)] = np.inf
+    times[small_mask & (real_mag <= abs_or_rel_cutoff)] = np.inf
 
     # now find the minimum time and the index of the responsible polynomial
     times = np.min(times.real, axis=1)
