f468c6115f9de7f1fbd25eeba7840975c85999ba
Evan Kiefl <kiefl.evan@gmail.com>
2023-06-14 15:47:17 -0700

    Add a break_forever.py script

---
+++ sandbox/break_forever.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+"""This sets up a 9-ball break forever and ever"""
+
+import numpy as np
+
+import pooltool as pt
+
+
+def main(args):
+    count = 0
+    while True:
+        shot = pt.System(
+            cue=pt.Cue(cue_ball_id="cue"),
+            table=(table := pt.Table.billiard_table()),
+            balls=pt.get_nine_ball_rack(
+                table, ordered=True, spacing_factor=args.spacing_factor
+            ),
+        )
+
+        # Aim at the head ball then strike the cue ball
+        shot.aim_at_ball(ball_id="1", cut=0)
+        shot.strike(V0=args.V0)
+
+        # Evolve the shot
+        pt.simulate(shot)
+
+        count += 1
+        print(count)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    ap = argparse.ArgumentParser("A good old 9-ball break")
+    ap.add_argument(
+        "--spacing-factor",
+        type=float,
+        default=1e-3,
+        help="What fraction of the ball radius should each ball be randomly separated by in the rack?",
+    )
+    ap.add_argument(
+        "--V0",
+        type=float,
+        default=8,
+        help="With what speed should the cue stick strike the cue ball?",
+    )
+
+    args = ap.parse_args()
+
+    main(args)
