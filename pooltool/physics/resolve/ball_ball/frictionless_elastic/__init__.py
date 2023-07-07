7d58011b768b2c5ea25be028bac87c1c55d0f575
Evan Kiefl <kiefl.evan@gmail.com>
2023-07-07 15:24:31 -0700

    Move kiss logic into separate method

--- pooltool/physics/resolve/ball_ball/frictionless_elastic/__init__.py
+++ pooltool/physics/resolve/ball_ball/frictionless_elastic/__init__.py
@@ -8,23 +8,13 @@ from pooltool.objects.ball.datatypes import Ball, BallState
 
 
 def _resolve_ball_ball(rvw1, rvw2, R):
-    """Frictionless, instantaneous, elastic, equal mass collision
-
-    NOTE A correction is made such that if the balls are not 2*R apart, they are moved
-    equally along their line of centers such that they are. To avoid float precision
-    round-off error, a small epsilon of additional distance (constants.EPS_SPACE) is put
-    between them, ensuring the balls are non-intersecting.
-    """
+    """Frictionless, instantaneous, elastic, equal mass collision"""
 
     r1, r2 = rvw1[0], rvw2[0]
     v1, v2 = rvw1[1], rvw2[1]
 
     n = math.unit_vector(r2 - r1)
     t = math.coordinate_rotation(n, np.pi / 2)
-
-    correction = 2 * R - math.norm3d(r2 - r1) + const.EPS_SPACE
-    rvw2[0] += correction / 2 * n
-    rvw1[0] -= correction / 2 * n
 
     v_rel = v1 - v2
     v_mag = math.norm3d(v_rel)
@@ -37,27 +27,40 @@ def _resolve_ball_ball(rvw1, rvw2, R):
     return rvw1, rvw2
 
 
-def resolve_ball_ball(
-    ball1: Ball, ball2: Ball, inplace: bool = False
-) -> Tuple[Ball, Ball]:
-    if not inplace:
-        ball1 = ball1.copy()
-        ball2 = ball2.copy()
-
-    rvw1, rvw2 = _resolve_ball_ball(
-        ball1.state.rvw.copy(),
-        ball2.state.rvw.copy(),
-        ball1.params.R,
-    )
-
-    ball1.state = BallState(rvw1, const.sliding)
-    ball2.state = BallState(rvw2, const.sliding)
-
-    return ball1, ball2
-
-
 class FrictionlessElastic:
     def resolve(
         self, ball1: Ball, ball2: Ball, inplace: bool = False
     ) -> Tuple[Ball, Ball]:
-        return resolve_ball_ball(ball1, ball2, inplace=inplace)
+        if not inplace:
+            ball1 = ball1.copy()
+            ball2 = ball2.copy()
+
+        ball1, ball2 = self.make_kiss(ball1, ball2)
+
+        rvw1, rvw2 = _resolve_ball_ball(
+            ball1.state.rvw.copy(),
+            ball2.state.rvw.copy(),
+            ball1.params.R,
+        )
+
+        ball1.state = BallState(rvw1, const.sliding)
+        ball2.state = BallState(rvw2, const.sliding)
+
+        return ball1, ball2
+
+    def make_kiss(self, ball1: Ball, ball2: Ball) -> Tuple[Ball, Ball]:
+        """Translate the balls so they are (almost) touching
+
+        This makes a correction such that if the balls are not 2*R apart, they are moved
+        equally along their line of centers such that they are. To avoid float precision
+        round-off error, a small epsilon of additional distance (constants.EPS_SPACE) is
+        put between them, ensuring the balls are non-intersecting.
+        """
+        r1, r2 = ball1.state.rvw[0], ball2.state.rvw[0]
+        n = math.unit_vector(r2 - r1)
+
+        correction = 2 * ball1.params.R - math.norm3d(r2 - r1) + const.EPS_SPACE
+        ball2.state.rvw[0] += correction / 2 * n
+        ball1.state.rvw[0] -= correction / 2 * n
+
+        return ball1, ball2
