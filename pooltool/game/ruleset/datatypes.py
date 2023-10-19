cddd29124d1fe9212dc8a1c821ef22e11b5fd865
Evan Kiefl <kiefl.evan@gmail.com>
2023-10-18 18:43:29 -0700

    Add some dataclasses

--- pooltool/game/ruleset/datatypes.py
+++ pooltool/game/ruleset/datatypes.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python
+from __future__ import annotations
 
 import uuid
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import Counter, Dict, List, Optional, Tuple
+
+import attrs
 
 import pooltool.constants as c
 from pooltool.objects.ball.datatypes import Ball
@@ -16,7 +19,7 @@ class Log:
         self.timer = Timer()
         self.msgs = []
 
-    def add_msg(self, msg, sentiment="neutral", quiet=False):
+    def add_msg(self, msg, sentiment="neutral", quiet=False) -> None:
         self.msgs.append(
             {
                 "time": self.timer.timestamp(),
@@ -32,46 +35,41 @@ class Log:
             self.update = True
 
 
+@attrs.define
+class ShotInfo:
+    shooter: Player
+    is_legal: bool
+    is_turn_over: bool
+    ball_in_hand: List[str]
+    awarded_points: Dict[Player, int]
+
+
 class Ruleset(ABC):
-    is_call_pocket = None
-    is_call_ball = None
+    def __init__(
+        self,
+        is_call_pocket: bool,
+        is_call_ball: bool,
+        player_names: Optional[List[str]] = None,
+    ) -> None:
+        self.is_call_ball = is_call_ball
+        self.is_call_pocket = is_call_pocket
 
-    def __init__(self):
-        if self.is_call_pocket is None:
-            raise Exception(f"{self.__class__.__name__} needs is_call_pocket defined")
-        if self.is_call_ball is None:
-            raise Exception(f"{self.__class__.__name__} needs is_call_ball defined")
-
-        self.players: List[Player]
-        self.shot_number: int
-        self.turn_number: int
-        self.active_player: Player
-        self.game_over: bool
-        self.winner: Player
+        self.points: Counter = Counter()
         self.tie: bool = False
+        self.game_over = False
+        self.shot_number: int = 0
+        self.turn_number: int = 0
         self.ball_call: Ball = Ball.dummy()
         self.pocket_call: Pocket = Pocket.dummy()
-        self.update_player_stats: bool = True
+        self.log: Log = Log()
 
-        self.log = Log()
+        self.shot_info: ShotInfo
+        self.active_player: Player
+        self.last_player: Player
+        self.winner: Player
 
-    def create_players(self, num_players):
-        self.players = []
-        for n in range(1, num_players + 1):
-            player = Player()
-            player.set_name(f"Player {n}")
-            self.players.append(player)
-
-    def init(self):
-        self.shot_number = 0
-        self.turn_number = 0
+        self.players: List[Player] = Player.create_players(player_names)
         self.set_next_player()
-        self.game_over = False
-        self.tie = False
-
-    def player_order(self):
-        for i in range(len(self.players)):
-            yield self.players[(self.turn_number + i) % len(self.players)]
 
     def set_next_player(self):
         next_player = self.players[self.turn_number % len(self.players)]
@@ -83,22 +81,7 @@ class Ruleset(ABC):
 
             self.log.add_msg(f"{self.active_player.name} is up", sentiment="neutral")
 
-    def process_shot(self, shot):
-        self.shot_info = {}
-        self.shot_info["is_legal"], self.shot_info["reason"] = self.legality(shot)
-        if self.shot_info["is_legal"]:
-            self.log.add_msg("The shot was legal.", quiet=True)
-        else:
-            self.log.add_msg(
-                f"Illegal shot! {self.shot_info['reason']}", sentiment="bad"
-            )
-        self.shot_info["is_turn_over"] = self.is_turn_over(shot)
-
-        self.award_points(shot)
-        self.award_ball_in_hand(shot)
-        self.respot_balls(shot)
-
-    def respot(self, shot, ball_id, x, y, z):
+    def respot(self, shot: System, ball_id: str, x: float, y: float, z: float):
         """Move cue ball to head spot
 
         Notes
@@ -108,76 +91,100 @@ class Ruleset(ABC):
         shot.balls[ball_id].state.rvw[0] = [x, y, z]
         shot.balls[ball_id].state.s = c.stationary
 
-    def advance(self, shot):
-        for player in self.players:
-            player.points += self.shot_info["points"][player]
-            self.log.add_msg(f"{player.name} points: {player.points}", quiet=True)
+    def process_shot(self, shot: System):
+        is_legal, reason, ball_in_hand = self.legality(shot)
+        is_turn_over = self.is_turn_over(shot)
+        awarded_points = self.award_points(shot)
 
+        self.shot_info = ShotInfo(
+            self.active_player,
+            is_legal,
+            is_turn_over,
+            ball_in_hand,
+            awarded_points,
+        )
+        self.points += awarded_points
+
+        if not is_legal:
+            self.log.add_msg(f"Illegal shot! {reason}", sentiment="bad")
+
+        self.award_points(shot)
+        self.award_ball_in_hand(shot)
+        self.respot_balls(shot)
+
+    def advance(self, shot):
         if self.is_game_over(shot):
             self.game_over = True
             self.decide_winner(shot)
             self.log.add_msg(f"Game over! {self.winner.name} wins!", sentiment="good")
             return
 
-        if self.shot_info["is_turn_over"]:
+        if self.shot_info.is_turn_over:
             self.turn_number += 1
         self.shot_number += 1
 
         self.active_player.ball_in_hand = []
         self.set_next_player()
-        if self.shot_info["ball_in_hand"] is not None:
-            self.active_player.ball_in_hand = self.shot_info["ball_in_hand"]
 
-        self.update_player_stats = True
+        if not self.shot_info.is_legal:
+            self.active_player.ball_in_hand = self.shot_info.ball_in_hand
 
         self.ball_call = Ball.dummy()
         self.pocket_call = Pocket.dummy()
 
     @abstractmethod
-    def legality(self, shot) -> Tuple[bool, str]:
+    def legality(self, shot: System) -> Tuple[bool, str, List[str]]:
         pass
 
     @abstractmethod
-    def award_points(self, shot):
+    def award_points(self, shot: System) -> Counter[Player]:
         pass
 
     @abstractmethod
-    def respot_balls(self, shot):
+    def respot_balls(self, shot: System):
         pass
 
     @abstractmethod
-    def is_game_over(self, shot):
+    def is_game_over(self, shot: System) -> bool:
         pass
 
     @abstractmethod
-    def award_ball_in_hand(self, shot):
+    def award_ball_in_hand(self, shot: System):
         pass
 
     @abstractmethod
-    def is_turn_over(self, shot):
+    def is_turn_over(self, shot: System) -> bool:
         pass
 
     @abstractmethod
-    def decide_winner(self, shot):
+    def decide_winner(self, shot: System):
         pass
 
     @abstractmethod
-    def get_initial_cueing_ball(self, balls):
+    def get_initial_cueing_ball(self, balls) -> Ball:
         pass
 
     @abstractmethod
-    def start(self, shot: System):
+    def start(self, shot: Optional[System] = None):
         pass
 
 
+def _get_id() -> str:
+    return uuid.uuid4().hex
+
+
+@attrs.define
 class Player:
-    def __init__(self):
-        self.id = uuid.uuid4().hex
-        self.name = None
-        self.is_shooting = False
-        self.points = 0
-        self.target_balls = []
-        self.ball_in_hand = []
+    name: str
+    is_shooting: bool = attrs.field(default=False)
+    target_balls: List[str] = attrs.field(factory=list)
+    ball_in_hand: List[str] = attrs.field(factory=list)
 
-    def set_name(self, name):
-        self.name = name
+    id: str = attrs.field(factory=_get_id, init=False)
+
+    @classmethod
+    def create_players(cls, names: Optional[List[str]] = None) -> List[Player]:
+        if names is None:
+            names = ["Player 1", "Player 2"]
+
+        return [cls(name) for name in names]
