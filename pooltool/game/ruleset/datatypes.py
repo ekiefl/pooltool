fa544aa0ba7e655f90dc054924014a08ccb432db
Evan Kiefl <kiefl.evan@gmail.com>
2023-11-07 13:48:29 -0800

    Add docstrings for Ruleset base class

--- pooltool/game/ruleset/datatypes.py
+++ pooltool/game/ruleset/datatypes.py
@@ -2,11 +2,10 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Counter, Dict, Generator, List, Optional, Tuple
+from typing import Counter, Generator, List, Optional, Tuple
 
 import attrs
 
-from pooltool.objects.ball.datatypes import Ball
 from pooltool.system.datatypes import Balls, System
 from pooltool.terminal import Timer
 from pooltool.utils.strenum import StrEnum, auto
@@ -56,6 +55,32 @@ class BallInHandOptions(StrEnum):
 
 @attrs.define
 class ShotConstraints:
+    """Constraints for a yet-to-happen shot
+
+    Attributes:
+        ball_in_hand:
+            Enum specifying if and how the player can place the cue ball by hand.
+        movable:
+            A list of identifiers for balls that the player is allowed to move
+            before the shot. If None, all balls are considered movable.
+        cueable:
+            A list of identifiers for balls that can be struck by the cue ball
+            during the shot. If None, all balls are considered cueable.
+        hittable:
+            A tuple of identifiers for balls that must be hit for the shot to be
+            considered legal.
+        call_shot:
+            A boolean indicating whether the shot must be called (i.e., the player must
+            declare which ball they intend to pocket and in which pocket). If False,
+            ball_call and pocket_call need not be defined.
+        ball_call:
+            The identifier of the ball the player has called to be pocketed.
+            If None, no specific ball has been called.
+        pocket_call:
+            The identifier of the pocket the player has called for the ball to
+            be pocketed in. If None, no specific pocket has been called.
+    """
+
     ball_in_hand: BallInHandOptions
     movable: Optional[List[str]]
     cueable: Optional[List[str]]
@@ -88,6 +113,13 @@ class ShotInfo:
 
 
 class Ruleset(ABC):
+    """Abstract base class for a pool game ruleset.
+
+    This class defines the skeleton of a pool game ruleset, including player management,
+    score tracking, and shot handling. Concrete subclasses must implement the abstract
+    methods to specify the behavior for specific pool games.
+    """
+
     def __init__(self, player_names: Optional[List[str]] = None) -> None:
         # Game progress tracking
         self.score: Counter = Counter()
@@ -110,10 +142,20 @@ class Ruleset(ABC):
 
     @property
     def last_player(self) -> Player:
+        """Returns the last player who played before the active player
+
+        If no turns have occurred, meaning the active player is the first player to
+        shoot in the game, this erroneously returns the last player in the player order.
+        """
         last_idx = (self.active_idx - 1) % len(self.players)
         return self.players[last_idx]
 
     def set_next_player(self):
+        """Sets the index for the next player
+
+        It is through this index that self.last_player and self.active_player are
+        defined from.
+        """
         self.active_idx = self.turn_number % len(self.players)
 
     def player_order(self) -> Generator[Player, None, None]:
@@ -122,11 +164,21 @@ class Ruleset(ABC):
             yield self.players[(self.turn_number + i) % len(self.players)]
 
     def process_shot(self, shot: System):
+        """Processes the information of the shot just played
+
+        Args:
+            shot: The shot data from the system.
+        """
         self.shot_info = self.build_shot_info(shot)
         self.score = self.get_score(shot)
         self.respot_balls(shot)
 
     def advance(self, shot: System):
+        """Advances the game state after a shot has been made and processed
+
+        Args:
+            shot: The shot data from the system.
+        """
         if self.shot_info.game_over:
             if (winner := self.shot_info.winner) is not None:
                 self.log.add_msg(f"Game over! {winner.name} wins!", sentiment="good")
@@ -144,19 +196,40 @@ class Ruleset(ABC):
 
     @abstractmethod
     def build_shot_info(self, shot: System) -> ShotInfo:
-        """Build up the essential information about the shot
+        """Construct the ShotInfo object for the current shot
 
-        The returned ShotInfo is used in conjunction with ShotConstraints to process the
-        shot (self.process_shot) and advance the game (self.advance).
+        This method evaluates the legality of a shot, determines if the turn or game is
+        over, and if applicable, decides the winner.
+
+        Args:
+            shot: The current shot being played.
+
+        Returns:
+            A ShotInfo instance containing details about the legality of the shot,
+            whether the turn and game are over, and who the winner is, if there is one.
         """
 
     @abstractmethod
     def next_shot_constraints(self, shot: System) -> ShotConstraints:
-        pass
+        """Determine the constraints for the next shot based on the current game state.
+
+        The method sets the conditions under which the next shot will be played, such as
+        whether ball-in-hand rules apply and which balls are legally hittable.
+
+        Args:
+            shot: The current shot being played.
+
+        Returns:
+            A ShotConstraints instance representing the rules for the next shot.
+        """
 
     @abstractmethod
     def initial_shot_constraints(self) -> ShotConstraints:
-        pass
+        """Define the initial constraints for the first shot of the game.
+
+        Returns:
+            A ShotConstraints instance with predefined constraints for the initial shot.
+        """
 
     @abstractmethod
     def get_score(self, shot: System) -> Counter:
