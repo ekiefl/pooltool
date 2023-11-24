d37ce9cb0c0beed25d25e60fbaac69389d2dd7b8
Evan Kiefl <kiefl.evan@gmail.com>
2023-11-23 17:47:34 -0800

    Update AimNaiveAI with new aim.at_* functions

--- pooltool/ai/bot/aim_naive/core.py
+++ pooltool/ai/bot/aim_naive/core.py
@@ -1,5 +1,8 @@
 from typing import Callable, Optional
 
+import numpy as np
+
+import pooltool.ai.aim as aim
 from pooltool.ai.action import Action
 from pooltool.game.ruleset.datatypes import Ruleset
 from pooltool.game.ruleset.utils import StateProbe, get_lowest_ball
@@ -14,19 +17,17 @@ class AimNaiveAI:
         game: Ruleset,
         callback: Optional[Callable[[Action], None]] = None,
     ) -> Action:
-        lowest_ball = get_lowest_ball(system, when=StateProbe.CURRENT)
+        action = Action(
+            V0=np.random.uniform(0.5, 4),
+            phi=np.random.uniform(0, 360),
+            theta=np.random.uniform(0, 0),
+            a=np.random.uniform(-0.5, 0.5),
+            b=np.random.uniform(-0.5, 0.5),
+        )
 
-        # Shortcoming of the System dataclass API, here. I have a System method
-        # `aim_at_ball` that sets the cue's parameters to aim at the ball with a desired
-        # cut angle. But `decide` is not supposed to set the cue state, it's just
-        # supposed to return the action. So to get around this, I store the cue state,
-        # call `aim_at_ball`, get the action, then set the cue state back to what it was
-        # originally.
-        original_action = Action.from_cue(system.cue)
-        system.aim_at_ball(lowest_ball.id)
-        action = Action.from_cue(system.cue)
+        lowest_ball = get_lowest_ball(system, when=StateProbe.CURRENT)
+        action.phi = aim.at_ball(system, lowest_ball.id)
         action.phi = wiggle(action.phi, 2)
-        original_action.apply(system.cue)
 
         return action
 
