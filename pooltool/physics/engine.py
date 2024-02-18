736625077fcb74d5630b92c6ecfd4c4d153bd5e1
Evan Kiefl <kiefl.evan@gmail.com>
2024-02-17 22:52:02 -0800

    PhysicsEngine descr

--- pooltool/physics/engine.py
+++ pooltool/physics/engine.py
@@ -7,7 +7,15 @@ from pooltool.physics.resolve import Resolver
 
 @attrs.define
 class PhysicsEngine:
-    """A billiards physics engine
+    """A billiards engine for pluggable physics.
 
-    This object """
+    Important:
+        Currently, only event resolution is a part of this class. The sliding, rolling,
+        and spinning ball trajectory evolution is currently "hard-coded", however can in
+        theory be added to this class to enable alternative trajectory models.
+
+    Attributes:
+        resolver:
+            The physics engine responsible for resolving events.
+    """
     resolver: Resolver = attrs.field(factory=Resolver.default)
