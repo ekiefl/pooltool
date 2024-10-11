6a8f43035da7fab0820752f0732cd7a2ffd63176
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2024-10-11 09:15:59 -0400

    rename omega -> w

--- pooltool/physics/resolve/ball_ball/frictional_mathavan/__init__.py
+++ pooltool/physics/resolve/ball_ball/frictional_mathavan/__init__.py
@@ -9,25 +9,25 @@ from pooltool.physics.resolve.ball_ball.core import CoreBallBallCollision
 
 
 def _resolve_ball_ball(rvw1, rvw2, R, m):
-    r_i, v_i, omega_i = rvw1.copy()
-    r_j, v_j, omega_j = rvw2.copy()
+    r_i, v_i, w_i = rvw1.copy()
+    r_j, v_j, w_j = rvw2.copy()
 
     r_i[2], r_i[1] = r_i[1], r_i[2]
     v_i[2], v_i[1] = v_i[1], 0
-    omega_i[2], omega_i[1] = omega_i[1], omega_i[2]
+    w_i[2], w_i[1] = w_i[1], w_i[2]
 
     r_j[2], r_j[1] = r_j[1], r_j[2]
     v_j[2], v_j[1] = v_j[1], 0
-    omega_j[2], omega_j[1] = omega_j[1], omega_j[2]
+    w_j[2], w_j[1] = w_j[1], w_j[2]
 
-    v_i1, omega_i1, v_j1, omega_j1 = _collide_balls(r_i, v_i, omega_i, r_j, v_j, omega_j, R, m)
+    v_i1, w_i1, v_j1, w_j1 = _collide_balls(r_i, v_i, w_i, r_j, v_j, w_j, R, m)
 
     rvw1[1,:2] = v_i1[::2]
     rvw2[1,:2] = v_j1[::2]
-    omega_i1[1], omega_i1[2] = omega_i1[2], omega_i1[1]
-    omega_j1[1], omega_j1[2] = omega_j1[2], omega_j1[1]
-    rvw1[2] = omega_i1
-    rvw2[2] = omega_j1
+    w_i1[1], w_i1[2] = w_i1[2], w_i1[1]
+    w_j1[1], w_j1[2] = w_j1[2], w_j1[1]
+    rvw1[2] = w_i1
+    rvw2[2] = w_j1
     return rvw1, rvw2
 
 
@@ -59,12 +59,12 @@ INF = float('inf')
 z_loc = array([0, 1, 0], dtype=np.float64)
 
 @jit(nopython=True, cache=const.use_numba_cache)
-def _collide_balls(r_i, v_i, omega_i,
-                   r_j, v_j, omega_j,
+def _collide_balls(r_i, v_i, w_i,
+                   r_j, v_j, w_j,
                    R, M,
                    mu_s=0.21,
                    mu_b=0.05,
-                   e=0.89,
+                   e=0.92,
                    deltaP=None):
     r_ij = r_j - r_i
     r_ij_mag_sqrd = dot(r_ij, r_ij)
@@ -76,14 +76,14 @@ def _collide_balls(r_i, v_i, omega_i,
     G = np.vstack((x_loc, y_loc, z_loc))
     v_ix, v_iy = dot(v_i, x_loc), dot(v_i, y_loc)
     v_jx, v_jy = dot(v_j, x_loc), dot(v_j, y_loc)
-    omega_ix, omega_iy, omega_iz = dot(G, omega_i)
-    omega_jx, omega_jy, omega_jz = dot(G, omega_j)
-    u_iR_x, u_iR_y = v_ix + R*omega_iy, v_iy - R*omega_ix
-    u_jR_x, u_jR_y = v_jx + R*omega_jy, v_jy - R*omega_jx
+    w_ix, w_iy, w_iz = dot(G, w_i)
+    w_jx, w_jy, w_jz = dot(G, w_j)
+    u_iR_x, u_iR_y = v_ix + R*w_iy, v_iy - R*w_ix
+    u_jR_x, u_jR_y = v_jx + R*w_jy, v_jy - R*w_jx
     u_iR_xy_mag = sqrt(u_iR_x**2 + u_iR_y**2)
     u_jR_xy_mag = sqrt(u_jR_x**2 + u_jR_y**2)
-    u_ijC_x = v_ix - v_jx - R*(omega_iz + omega_jz)
-    u_ijC_z = R*(omega_ix + omega_jx)
+    u_ijC_x = v_ix - v_jx - R*(w_iz + w_jz)
+    u_ijC_z = R*(w_ix + w_jx)
     u_ijC_xz_mag = sqrt(u_ijC_x**2 + u_ijC_z**2)
     v_ijy = v_jy - v_iy
     if deltaP is None:
@@ -138,20 +138,20 @@ def _collide_balls(r_i, v_i, omega_i,
         v_iy += deltaV_iy
         v_jy += deltaV_jy
         # update angular velocities:
-        omega_ix += deltaOm_ix
-        omega_iy += deltaOm_iy
-        omega_iz += deltaOm_iz
-        omega_jx += deltaOm_j[0]
-        omega_jy += deltaOm_j[1]
-        omega_jz += deltaOm_j[2]
+        w_ix += deltaOm_ix
+        w_iy += deltaOm_iy
+        w_iz += deltaOm_iz
+        w_jx += deltaOm_j[0]
+        w_jy += deltaOm_j[1]
+        w_jz += deltaOm_j[2]
         # update ball-table slips:
-        u_iR_x, u_iR_y = v_ix + R*omega_iy, v_iy - R*omega_ix
-        u_jR_x, u_jR_y = v_jx + R*omega_jy, v_jy - R*omega_jx
+        u_iR_x, u_iR_y = v_ix + R*w_iy, v_iy - R*w_ix
+        u_jR_x, u_jR_y = v_jx + R*w_jy, v_jy - R*w_jx
         u_iR_xy_mag = sqrt(u_iR_x**2 + u_iR_y**2)
         u_jR_xy_mag = sqrt(u_jR_x**2 + u_jR_y**2)
         # update ball-ball slip:
-        u_ijC_x = v_ix - v_jx - R*(omega_iz + omega_jz)
-        u_ijC_z = R*(omega_ix + omega_jx)
+        u_ijC_x = v_ix - v_jx - R*(w_iz + w_jz)
+        u_ijC_z = R*(w_ix + w_jx)
         u_ijC_xz_mag = sqrt(u_ijC_x**2 + u_ijC_z**2)
         # increment work:
         v_ijy0 = v_ijy
@@ -175,7 +175,7 @@ def _collide_balls(r_i, v_i, omega_i,
     # ''', niters)
     v_i = array((v_ix, v_iy, 0))
     v_j = array((v_jx, v_jy, 0))
-    omega_i = array((omega_ix, omega_iy, omega_iz))
-    omega_j = array((omega_jx, omega_jy, omega_jz))
+    w_i = array((w_ix, w_iy, w_iz))
+    w_j = array((w_jx, w_jy, w_jz))
     G_T = G.T
-    return dot(G_T, v_i), dot(G_T, omega_i), dot(G_T, v_j), dot(G_T, omega_j)
+    return dot(G_T, v_i), dot(G_T, w_i), dot(G_T, v_j), dot(G_T, w_j)
