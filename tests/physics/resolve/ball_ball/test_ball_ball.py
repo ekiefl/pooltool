5546c94d04a62ffcf3025d074b24ab05084adc06
Evan Kiefl <kiefl.evan@gmail.com>
2024-11-03 22:01:22 -0800

    Parameterize magnitude of zspin

--- tests/physics/resolve/ball_ball/test_ball_ball.py
+++ tests/physics/resolve/ball_ball/test_ball_ball.py
@@ -52,31 +52,16 @@ def test_head_on_zero_spin_inelastic(model: BallBallCollisionStrategy):
     # Object ball should have +x velocity
     assert ob_f.state.rvw[1][0] > 0
 
-
-@pytest.mark.parametrize("model", [FrictionalInelastic(), FrictionalMathavan()])
-def test_head_on_z_spin(model: BallBallCollisionStrategy):
-    """Cue ball has positive z-spin (e.g. hitting right-hand-side of cue ball)"""
-    cb_i, ob_i = head_on()
-    cb_i.state.rvw[2][2] = (cb_wz_i := 0.1)
-
-    cb_f, ob_f = model.resolve(cb_i, ob_i, inplace=False)
-
-    assert ob_f.state.rvw[1][1] > 0, "Object ball should be thrown left"
-
-    ob_wz_f = ob_f.state.rvw[2][2]
-    opposing_zspin = np.sign(ob_wz_f) == -np.sign(cb_wz_i)
-    assert opposing_zspin, "Final OB z-spin should oppose initial CB z-spin"
-
-    cb_wz_f = cb_f.state.rvw[2][2]
-    assert cb_wz_f > 0, "Spin direction shouldn't reverse"
-    assert cb_wz_f < cb_wz_i, "Spin should be decay"
+    # Cue ball should have +x velocity, too (because of inelasticity)
+    assert cb_f.state.rvw[1][0] > 0
 
 
 @pytest.mark.parametrize("model", [FrictionalInelastic(), FrictionalMathavan()])
-def test_head_on_high_z_spin(model: BallBallCollisionStrategy):
+@pytest.mark.parametrize("cb_wz_i", [0.1, 1, 10, 100])
+def test_head_on_z_spin(model: BallBallCollisionStrategy, cb_wz_i: float):
     """Cue ball has positive z-spin (e.g. hitting right-hand-side of cue ball)"""
     cb_i, ob_i = head_on()
-    cb_i.state.rvw[2][2] = (cb_wz_i := 100)
+    cb_i.state.rvw[2][2] = cb_wz_i
 
     cb_f, ob_f = model.resolve(cb_i, ob_i, inplace=False)
 
