f6a7296fe0872a1615a687e2a3b5b052900b20a1
Evan Kiefl <kiefl.evan@gmail.com>
2024-12-10 22:55:23 -0800

    norm3d calls norm3d_squared
    - There is essentially no overhead cost associated with function calls
      when a numba fn calls a numba fn. This is because numba can directly
      call the compiled code without involving the python interpreter

--- pooltool/ptmath/utils.py
+++ pooltool/ptmath/utils.py
@@ -244,7 +244,7 @@ def norm3d(vec: NDArray[np.float64]) -> float:
     2.65 µs ± 63 ns per loop (mean ± std. dev. of 7 runs, 100,000 loops each)
     241 ns ± 2.57 ns per loop (mean ± std. dev. of 7 runs, 1,000,000 loops each)
     """
-    return sqrt(vec[0] ** 2 + vec[1] ** 2 + vec[2] ** 2)
+    return sqrt(norm3d_squared(vec))
 
 
 @jit(nopython=True, cache=const.use_numba_cache)
