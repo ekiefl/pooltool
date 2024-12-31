c48dd43748c007b495e5e2a537c06c9aff9800df
Evan Kiefl <kiefl.evan@gmail.com>
2024-12-31 13:21:37 -0800

    Support nested ModelArgs

--- pooltool/physics/resolve/types.py
+++ pooltool/physics/resolve/types.py
@@ -1,12 +1,24 @@
-from typing import Mapping, Union
+from typing import ForwardRef, Mapping, Union
 
 from pooltool.serialize import conversion
+from pooltool.serialize.serializers import SerializeFormat
 
 ArgType = Union[float, int, str, bool]
 """Allowable types model arguments"""
 
-ModelArgs = Mapping[str, ArgType]
+# Recursive type definition is not fixed with PEP 563, so ForwardRef type is required.
+ModelArgs = Mapping[str, Union["ModelArgs", ArgType]]
 """A mapping of argument names to argument values"""
+
+# https://github.com/python-attrs/cattrs/issues/201
+ModelArgs.__args__[1].__args__[0]._evaluate(globals(), locals(), recursive_guard=set())  # type: ignore
+conversion.register_structure_hook_func(
+    lambda t: t.__class__ is ForwardRef,
+    lambda v, t: conversion.converters[SerializeFormat.YAML].structure(
+        v, t.__forward_value__
+    ),
+    which=(SerializeFormat.YAML,),
+)
 
 # Leave type-casting to the JSON/YAML serializer
 conversion.register_structure_hook(cl=ArgType, func=lambda d, t: d)
---
+++ tests/physics/resolve/test_types.py
@@ -0,0 +1,46 @@
+import pytest
+
+from pooltool.physics.resolve.types import ModelArgs
+from pooltool.serialize import conversion
+
+
+def test_model_args_flat(tmp_path):
+    d = {
+        "a": 42,
+        "b": True,
+        "c": "string",
+    }
+    conversion.unstructure_to(d, tmp_path / "test.yaml")
+    assert d == conversion.structure_from(tmp_path / "test.yaml", ModelArgs)
+
+
+def test_model_args_recursive(tmp_path):
+    d = {
+        "a": 42,
+        "b": True,
+        "c": {
+            "1": 42,
+            "2": True,
+            "3": {
+                "x": 42,
+                "y": True,
+                "z": "string",
+            },
+        },
+    }
+    conversion.unstructure_to(d, tmp_path / "test.yaml")
+    assert d == conversion.structure_from(tmp_path / "test.yaml", ModelArgs)
+
+
+def test_model_args_bad_keys(tmp_path):
+    # Only string keys  allowed
+    d = {("a", "b"): 1}
+    with pytest.raises(Exception):
+        assert d != conversion.unstructure_to(d, tmp_path / "test.yaml")
+
+    # Unfortunately ints are valid because of YAML type casting
+    d = {42: 1}
+    conversion.unstructure_to(d, tmp_path / "test.yaml")
+    with pytest.raises(Exception):
+        # But at least round trip fails.
+        assert d == conversion.structure_from(tmp_path / "test.yaml", ModelArgs)
