a3761c8928177534cf904fdc9f085b2fb21a2d58
erdo100 <ersin.dogan73@gmail.com>
2025-01-03 19:09:54 +0100

    Added 3 test shots.

--- tests/ruleset/test_three_cushion.py
+++ tests/ruleset/test_three_cushion.py
@@ -1,3 +1,36 @@
 def test_three_cushion():
+# 03.01.2024: Continue working here
+#    def load(cls, path: Pathish) -> System:
+        """Load a System from a file in a serialized format.
+
+        Supported file extensions:
+
+        (1) ``.json``
+        (2) ``.msgpack``
+
+        Args:
+            path:
+                Either a ``pathlib.Path`` object or a string representing the file path. The
+                extension should match the supported filetypes mentioned above.
+
+        Returns:
+            System: The deserialized System object loaded from the file.
+
+        Raises:
+            AssertionError: If the file specified by `path` does not exist.
+            ValueError: If the file extension is not supported.
+
+        Examples:
+
+        Please refer to the examples in :meth:`save`.
+
+        See Also:
+            Save systems with :meth:`save`.
+        """
+
+
     assert 1 == 0
+
     return
+
+
