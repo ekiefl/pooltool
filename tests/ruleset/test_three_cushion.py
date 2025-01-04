50c5bb74885e74dc20ec562b979baac9c9d05275
erdo100 <ersin.dogan73@gmail.com>
2025-01-04 03:48:44 +0100

    Edited test_three_cushion.py: corrected import of System for is_point call. Added path to test shot.

--- tests/ruleset/test_three_cushion.py
+++ tests/ruleset/test_three_cushion.py
@@ -1,36 +1,36 @@
-# from pooltool.ruleset.three_cushion import is_point
-# from pooltool.system.datatypes import load
+from pooltool.system.datatypes import System
 
 def test_three_cushion():
+
     # testing function ispoint() with predefined shots
-    shot = load("01_test_shot_no_point.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/01_test_shot_no_point.msgpack")
+    assert not is_point(shot)
 
-    shot = load("01a_test_shot_no_point.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/01a_test_shot_no_point.msgpack")
+    assert not is_point(shot)
 
-    shot = load("02_test_shot_ispoint.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/02_test_shot_ispoint.msgpack")
+    assert is_point(shot)
 
-    shot = load("02a_test_shot_ispoint.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/02a_test_shot_ispoint.msgpack")
+    assert is_point(shot)
 
-    shot = load("03_test_shot_ispoint.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/03_test_shot_ispoint.msgpack")
+    assert is_point(shot)
 
-    shot = load("03a_test_shot_ispoint.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/03a_test_shot_ispoint.msgpack")
+    assert is_point(shot)
 
-    shot = load("04_test_shot_no_point.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/04_test_shot_no_point.msgpack")
+    assert not is_point(shot)
 
-    shot = load("04a_test_shot_no_point.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/04a_test_shot_no_point.msgpack")
+    assert not is_point(shot)
 
-    shot = load("05_test_shot_ispoint.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/05_test_shot_ispoint.msgpack")
+    assert is_point(shot)
 
-    shot = load("05a_test_shot_ispoint.msgpack")
-    assert is_point(shot) == False
+    shot = System.load("test_shots/05a_test_shot_ispoint.msgpack")
+    assert is_point(shot)
 
 
