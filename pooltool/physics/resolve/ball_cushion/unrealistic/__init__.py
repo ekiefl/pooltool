9c12a6efa2b9d201d8cedfc75b1a83b8134dd7ec
Evan Kiefl <kiefl.evan@gmail.com>
2025-01-06 21:44:56 -0800

    Make attrs class and define model attribute

--- pooltool/physics/resolve/ball_cushion/unrealistic/__init__.py
+++ pooltool/physics/resolve/ball_cushion/unrealistic/__init__.py
@@ -2,12 +2,20 @@
 
 from typing import Tuple
 
+import attrs
+
 from pooltool.objects.ball.datatypes import Ball
 from pooltool.objects.table.components import LinearCushionSegment
 from pooltool.physics.resolve.ball_cushion.core import CoreBallLCushionCollision
+from pooltool.physics.resolve.models import BallLCushionModel
 
 
+@attrs.define
 class UnrealisticLinear(CoreBallLCushionCollision):
+    model: BallLCushionModel = attrs.field(
+        default=BallLCushionModel.UNREALISTIC, init=False, repr=False
+    )
+
     def solve(
         self, ball: Ball, cushion: LinearCushionSegment
     ) -> Tuple[Ball, LinearCushionSegment]:
