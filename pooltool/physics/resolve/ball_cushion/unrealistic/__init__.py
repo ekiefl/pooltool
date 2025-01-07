5ecddb2c0c010e3f058e666fd5a7fc1f10117638
Evan Kiefl <kiefl.evan@gmail.com>
2025-01-06 21:41:28 -0800

    Creating the class and `solve` method

--- pooltool/physics/resolve/ball_cushion/unrealistic/__init__.py
+++ pooltool/physics/resolve/ball_cushion/unrealistic/__init__.py
@@ -1,1 +1,14 @@
 """An unrealistic ball-cushion model"""
+
+from typing import Tuple
+
+from pooltool.objects.ball.datatypes import Ball
+from pooltool.objects.table.components import LinearCushionSegment
+from pooltool.physics.resolve.ball_cushion.core import CoreBallLCushionCollision
+
+
+class UnrealisticLinear(CoreBallLCushionCollision):
+    def solve(
+        self, ball: Ball, cushion: LinearCushionSegment
+    ) -> Tuple[Ball, LinearCushionSegment]:
+        return ball, cushion
