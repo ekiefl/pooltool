0ab0b3053a30734fe9dfbb45dd98176ee5161c81
Evan Kiefl <kiefl.evan@gmail.com>
2025-03-29 11:14:44 -0700

    interpolate ball states

--- pooltool/evolution/__init__.py
+++ pooltool/evolution/__init__.py
@@ -1,9 +1,10 @@
 """Shot evolution algorithm routines"""
 
-from pooltool.evolution.continuize import continuize
+from pooltool.evolution.continuize import continuize, interpolate_ball_states
 from pooltool.evolution.event_based.simulate import simulate
 
 __all__ = [
     "continuize",
     "simulate",
+    "interpolate_ball_states",
 ]
