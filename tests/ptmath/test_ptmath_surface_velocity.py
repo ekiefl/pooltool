b4f0e3402989674e23ede98f1bff2be4bf679434
Evan Kiefl <kiefl.evan@gmail.com>
2025-05-09 22:38:20 -0700

    More tests

--- tests/ptmath/test_ptmath_surface_velocity.py
+++ tests/ptmath/test_ptmath_surface_velocity.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from pooltool.ptmath.utils import surface_velocity, tangent_surface_velocity
 
@@ -56,3 +57,89 @@ def test_tangent_surface_velocity_no_linear_velocity():
     assert np.isclose(
         s_surface, s_surface_expected, atol=1e-6
     ), "with no linear velocity, tangent surface speed should equal magnitude of tangential angular velocity times radius"
+
+
+@pytest.mark.parametrize(
+    "v,w,d,expected_surface,expected_tangent",
+    [
+        (
+            # v -> +y, w -> +z, d -> +z
+            np.array([0.0, 1.0, 0.0]),
+            np.array([0.0, 0.0, 1.0]),
+            np.array([0.0, 0.0, 1.0]),
+            # Surface
+            np.array([0.0, 1.0, 0.0]),
+            # Surface tangent
+            np.array([0.0, 1.0, 0.0]),
+        ),
+        (
+            # v -> +y, w -> +z, d -> -z
+            np.array([0.0, 1.0, 0.0]),
+            np.array([0.0, 0.0, 1.0]),
+            np.array([0.0, 0.0, -1.0]),
+            # Surface
+            np.array([0.0, 1.0, 0.0]),
+            # Surface tangent
+            np.array([0.0, 1.0, 0.0]),
+        ),
+        (
+            # v -> +y, w -> +z, d -> +x
+            np.array([0.0, 1.0, 0.0]),
+            np.array([0.0, 0.0, 1.0]),
+            np.array([1.0, 0.0, 0.0]),
+            # Surface
+            np.array([0.0, 2.0, 0.0]),
+            # Surface tangent
+            np.array([0.0, 2.0, 0.0]),
+        ),
+        (
+            # v -> +y, w -> +z, d -> -x
+            np.array([0.0, 1.0, 0.0]),
+            np.array([0.0, 0.0, 1.0]),
+            np.array([-1.0, 0.0, 0.0]),
+            # Surface
+            np.array([0.0, 0.0, 0.0]),
+            # Surface tangent
+            np.array([0.0, 0.0, 0.0]),
+        ),
+        (
+            # v -> +y, w -> +z, d -> -y
+            np.array([0.0, 1.0, 0.0]),
+            np.array([0.0, 0.0, 1.0]),
+            np.array([0.0, -1.0, 0.0]),
+            # Surface
+            np.array([1.0, 1.0, 0.0]),
+            # Surface tangent
+            np.array([1.0, 0.0, 0.0]),
+        ),
+        (
+            # v -> +y & +z, w -> +z, d -> -y
+            np.array([0.0, 1.0, 1.0]),
+            np.array([0.0, 0.0, 1.0]),
+            np.array([0.0, -1.0, 0.0]),
+            # Surface
+            np.array([1.0, 1.0, 1.0]),
+            # Surface tangent
+            np.array([1.0, 0.0, 1.0]),
+        ),
+        (
+            # v -> +y & +z, w -> +z, d -> -z
+            np.array([0.0, 1.0, 1.0]),
+            np.array([0.0, 0.0, 1.0]),
+            np.array([0.0, 0.0, -1.0]),
+            # Surface
+            np.array([0.0, 1.0, 1.0]),
+            # Surface tangent
+            np.array([0.0, 1.0, 0.0]),
+        ),
+    ],
+)
+def test_smoke_test(v, w, d, expected_surface, expected_tangent):
+    R = 1
+    rvw = np.array([np.zeros(3), v, w], dtype=np.float64)
+
+    v_surface = surface_velocity(rvw, d, R)
+    assert np.isclose(v_surface, expected_surface).all()
+
+    v_tangent = tangent_surface_velocity(rvw, d, R)
+    assert np.isclose(v_tangent, expected_tangent).all()
