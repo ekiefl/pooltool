3c2df064e4954fdc3fb890099c6963f0851854fb
Evan Kiefl <kiefl.evan@gmail.com>
2025-05-10 11:08:55 -0700

    Add test recommended by code rabbit

--- tests/system/test_datatypes.py
+++ tests/system/test_datatypes.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pooltool.objects.ball.datatypes import Ball
 from pooltool.system.datatypes import System
 
 
@@ -14,3 +15,13 @@ def test_cue_ball_id_mismatch():
             balls=system.balls,
             table=system.table,
         )
+
+
+def test_system_raises_on_unequal_radii():
+    b1 = Ball.create("1", R=1.0)
+    b2 = Ball.create("2", R=1.5)
+
+    template = System.example()
+
+    with pytest.raises(AssertionError, match="different radius"):
+        System(balls={"1": b1, "2": b2}, cue=template.cue, table=template.table)
