5562ba9efece5022b10acf5a16102bd1c8bb866d
Evan Kiefl <kiefl.evan@gmail.com>
2025-06-29 15:21:02 -0700

    Could obfuscate user errors

--- pooltool/main.py
+++ pooltool/main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-import sys
 
 import attrs
 import click
 
 from pooltool.ani.animate import Game, ShowBaseConfig
-from pooltool.error import ConfigError, SimulateError, StrokeError
 
 
 @click.command()
@@ -19,14 +17,4 @@ def run(monitor):
 
 
 if __name__ == "__main__":
-    try:
-        run()
-    except ConfigError as e:
-        print(e)
-        sys.exit(1)
-    except StrokeError as e:
-        print(e)
-        sys.exit(1)
-    except SimulateError as e:
-        print(e)
-        sys.exit(1)
+    run()
