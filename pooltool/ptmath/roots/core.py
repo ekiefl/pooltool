f02b4f13ac17e2c2de9626eca4fadbbffe6bc620
Evan Kiefl <kiefl.evan@gmail.com>
2025-11-10 19:04:35 -0800

    Add single root set calculation

--- pooltool/ptmath/roots/core.py
+++ pooltool/ptmath/roots/core.py
@@ -1,7 +1,69 @@
 import numpy as np
+from numba import jit
 from numpy.typing import NDArray
 
+from pooltool import constants as const
 
+
+@jit(nopython=True, cache=const.use_numba_cache)
+def get_real_positive_smallest_root(
+    roots: NDArray[np.complex128],
+    abs_or_rel_cutoff: float = 1e-3,
+    rtol: float = 1e-3,
+    atol: float = 1e-9,
+) -> float:
+    """Returns the smallest positive and real root from a set of roots.
+
+    Args:
+        roots:
+            A 1D array of n polynomial roots.
+        abs_or_rel_cutoff:
+            The criteria for a root being real depends on the magnitude of its real
+            component. If it's large, we require the imaginary component to be less than
+            atol in absolute terms. But when the real component is small, we require the
+            imaginary component be less than a fraction, rtol, of the real component.
+            abs_or_rel_cutoff defines a threshold for the magnitude of the real
+            component, above which atol is used and below which rtol is used.
+        atol:
+            A root r (with abs(r.real) >= abs_or_rel_cutoff) is considered real if
+            abs(r.imag) < atol.
+        rtol:
+            A root r (with abs(r.real) < abs_or_rel_cutoff) is considered real if
+            abs(r.imag) / abs(r.real) < rtol. And in the special case when r.real == 0,
+            the root is considered real if r.imag == 0, too.
+
+    Returns:
+        The smallest root that is real and positive. If no such root exists (e.g. all
+        roots are complex or negative), then `np.inf` is returned.
+    """
+    min_root = np.inf
+
+    for i in range(len(roots)):
+        root = roots[i]
+        root_real = root.real
+        root_imag = root.imag
+
+        if root_real < 0.0:
+            continue
+
+        imag_mag = abs(root_imag)
+        real_mag = abs(root_real)
+
+        is_real = False
+        if real_mag > abs_or_rel_cutoff:
+            is_real = imag_mag < atol
+        elif real_mag > 0:
+            is_real = (imag_mag / real_mag) < rtol
+        else:
+            is_real = imag_mag == 0.0
+
+        if is_real and root_real < min_root:
+            min_root = root_real
+
+    return min_root
+
+
+@jit(nopython=True, cache=const.use_numba_cache)
 def get_real_positive_smallest_roots(
     roots: NDArray[np.complex128],
     abs_or_rel_cutoff: float = 1e-3,
@@ -34,25 +96,12 @@ def get_real_positive_smallest_roots(
             positive. If no such root exists (e.g. all roots are complex), then
             `np.inf` is used.
     """
-    positive = roots.real >= 0.0
+    num_sets = roots.shape[0]
+    result = np.empty(num_sets, dtype=np.float64)
 
-    imag_mag = np.abs(roots.imag)
-    real_mag = np.abs(roots.real)
+    for i in range(num_sets):
+        result[i] = get_real_positive_smallest_root(
+            roots[i, :], abs_or_rel_cutoff, rtol, atol
+        )
 
-    big = real_mag > abs_or_rel_cutoff
-    big_keep = (imag_mag < atol) & positive
-
-    small = real_mag <= abs_or_rel_cutoff
-    small_keep1 = (real_mag > 0) & ((imag_mag / real_mag) < rtol)
-    small_keep2 = (real_mag == 0) & (imag_mag == 0)
-    small_keep = (small_keep1 | small_keep2) & positive
-
-    is_real = (small & small_keep) | (big & big_keep)
-    processed_roots: NDArray[np.complex128] = np.where(
-        is_real, roots, np.complex128(np.inf)
-    )
-
-    # Find the minimum real positive root in each row
-    min_real_positive_roots = np.min(processed_roots.real, axis=1)
-
-    return min_real_positive_roots
+    return result
