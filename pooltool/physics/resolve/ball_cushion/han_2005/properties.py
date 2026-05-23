f8f227faa69f05d63a88fb343720ce98b89f0702
Evan Kiefl <kiefl.evan@gmail.com>
2026-05-22 17:50:49 -0700

    Touchup call sigs and docstrings

--- pooltool/physics/resolve/ball_cushion/han_2005/properties.py
+++ pooltool/physics/resolve/ball_cushion/han_2005/properties.py
@@ -1,35 +1,33 @@
 import numpy as np
+from numpy.typing import NDArray
 
 import pooltool.ptmath as ptmath
 
 
-def get_ball_cushion_restitution(rvw, e_c):
+def get_ball_cushion_restitution(rvw: NDArray[np.float64], e_c: float):
     """Get restitution coefficient dependent on ball state
 
-    Parameters
-    ==========
-    rvw: np.array
-        Assumed to be in reference frame such that <1,0,0> points
-        perpendicular to the cushion, and in the direction away from the table
+    Args:
+        rvw:
+            Assumed to be in reference frame such that <1,0,0> points perpendicular to
+            the cushion, and in the direction away from the table.
 
-    Notes
-    =====
-    - https://essay.utwente.nl/59134/1/scriptie_J_van_Balen.pdf suggests a constant
-      value of 0.85
+    Notes:
+        - https://essay.utwente.nl/59134/1/scriptie_J_van_Balen.pdf suggests a constant
+          value of 0.85
     """
 
     return e_c
     return max([0.40, 0.50 + 0.257 * rvw[1, 0] - 0.044 * rvw[1, 0] ** 2])
 
 
-def get_ball_cushion_friction(rvw, f_c):
+def get_ball_cushion_friction(rvw: NDArray[np.float64], f_c: float):
     """Get friction coeffecient depend on ball state
 
-    Parameters
-    ==========
-    rvw: np.array
-        Assumed to be in reference frame such that <1,0,0> points
-        perpendicular to the cushion, and in the direction away from the table
+    Args:
+        rvw:
+            Assumed to be in reference frame such that <1,0,0> points perpendicular to
+            the cushion, and in the direction away from the table.
     """
 
     ang = ptmath.angle(rvw[1])
