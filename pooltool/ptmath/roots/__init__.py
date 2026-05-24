4af6f7da85ab4c921d7ac677389efc341154b619
Evan Kiefl <kiefl.evan@gmail.com>
2026-05-24 00:30:58 -0700

    Isolate make_kiss test into own module

--- pooltool/ptmath/roots/__init__.py
+++ pooltool/ptmath/roots/__init__.py
@@ -3,12 +3,14 @@ import pooltool.ptmath.roots.quartic as quartic
 from pooltool.ptmath.roots.core import (
     get_real_positive_smallest_root,
     get_real_positive_smallest_roots,
+    get_real_smallest_magnitude_root,
     is_real_number,
 )
 
 __all__ = [
     "get_real_positive_smallest_root",
     "get_real_positive_smallest_roots",
+    "get_real_smallest_magnitude_root",
     "is_real_number",
     "quadratic",
     "quartic",
