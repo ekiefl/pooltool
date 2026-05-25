274326cef8acde8717c02780f9496941f83dec5a
Derek McBlane <mcblanederek@gmail.com>
2026-05-25 16:47:38 -0400

    first pass at 3d linear cushion collision detection

--- pooltool/evolution/event_based/detect/quartic_coefficients.py
+++ pooltool/evolution/event_based/detect/quartic_coefficients.py
@@ -28,3 +28,26 @@ def parabola_sphere_distance_quartic_coefficients(
             (xx[2] + yy[2] + zz[2]) / 2,
         ]
     )
+
+
+@jit(nopython=True, cache=const.use_numba_cache)
+def parabola_circle_distance_2d_quartic_coefficients(
+    xy: NDArray[np.float64],
+    center: NDArray[np.float64],
+    radius: float,
+) -> NDArray[np.float64]:
+
+    x = xy[0]
+    y = xy[1]
+
+    C_x = x[0] - center[0]
+    C_y = y[0] - center[1]
+    return np.array(
+        [
+            (C_x * C_x + C_y * C_y - radius * radius) / 2,
+            x[1] * C_x + y[1] * C_y,
+            x[2] * C_x + y[2] * C_y + (x[1] * x[1] + y[1] * y[1]) / 2,
+            x[2] * x[1] + y[2] * y[1],
+            (x[2] * x[2] + y[2] * y[2]) / 2,
+        ]
+    )
