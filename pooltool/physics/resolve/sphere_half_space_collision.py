c2293955cdeccdf0568bf2f093388aaa4f0dacc9
Derek McBlane <mcblanederek@gmail.com>
2026-09-04 23:16:53 -0400

    in resolve_sphere_half_space_collision, only rotate v and w, do not bother rotating r

--- pooltool/physics/resolve/sphere_half_space_collision.py
+++ pooltool/physics/resolve/sphere_half_space_collision.py
@@ -4,28 +4,26 @@ from numba import jit
 
 import pooltool.constants as const
 import pooltool.ptmath as ptmath
-from pooltool.physics.utils import tangent_surface_velocity
+from pooltool.physics.utils import tangent_surface_velocity_vw
 
 
 def resolve_sphere_half_space_collision(normal, rvw, R, mu_k, e):
     unit_z = np.array([0.0, 0.0, 1.0])
     frame_rotation = ptmath.quaternion_from_vector_to_vector(normal, unit_z)
-    rvw = quaternion.rotate_vectors(frame_rotation, rvw)
-    rvw = resolve_sphere_half_space_collision_z_normal(rvw=rvw, R=R, mu_k=mu_k, e=e)
-    rvw = quaternion.rotate_vectors(frame_rotation.conjugate(), rvw)
+    v_prime, w_prime = quaternion.rotate_vectors(frame_rotation, rvw[1:3])
+    v_prime, w_prime = resolve_sphere_half_space_collision_z_normal(
+        v_prime, w_prime, R=R, mu_k=mu_k, e=e
+    )
+    rvw[1:3] = quaternion.rotate_vectors(frame_rotation.conjugate(), [v_prime, w_prime])
     return rvw
 
 
 @jit(nopython=True, cache=const.use_numba_cache)
-def resolve_sphere_half_space_collision_z_normal(rvw, R, mu_k, e):
+def resolve_sphere_half_space_collision_z_normal(v_i, w_i, R, mu_k, e):
     unit_z = np.array([0.0, 0.0, 1.0])
 
-    v_i = rvw[1]
-    w_i = rvw[2]
-
-    rvw_f = rvw.copy()
-    v_f = rvw_f[1]
-    w_f = rvw_f[2]
+    v_f = v_i.copy()
+    w_f = w_i.copy()
 
     assert v_i[2] < 0
 
@@ -36,7 +34,7 @@ def resolve_sphere_half_space_collision_z_normal(rvw, R, mu_k, e):
     v_i[2] = 0.0
     w_i[2] = 0.0
 
-    v_c_i = tangent_surface_velocity(rvw, -unit_z, R)
+    v_c_i = tangent_surface_velocity_vw(v_i, w_i, -unit_z, R)
     v_c_i_magnitude = ptmath.norm3d(v_c_i)
 
     has_relative_velocity = v_c_i_magnitude > const.EPS
@@ -62,4 +60,4 @@ def resolve_sphere_half_space_collision_z_normal(rvw, R, mu_k, e):
             * ptmath.cross(unit_z, v_hat_c_i)
         )
 
-    return rvw_f
+    return v_f, w_f
